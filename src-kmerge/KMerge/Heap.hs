5bb509f24424653feb58988dca1215ed024fc3d0
Oleg Grenrus <oleg.grenrus@iki.fi>
2023-11-23 14:51:44 +0200

    Add a MutableLoserTree and MutableHeap implmenetations
    It seems that we need both the SPECIALIZE pragmas, and
    -fspecialize-aggressively.
    
    We really need to have both PrimMonad and Ord dictionaries to go away,
    otherwise loserTreeMerge will be ten times slower.

---
+++ src-kmerge/KMerge/Heap.hs
@@ -0,0 +1,181 @@
+{-# LANGUAGE BangPatterns        #-}
+{-# LANGUAGE CPP                 #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fexpose-all-unfoldings #-}
+-- | Mutable heap for k-merge algorithm.
+--
+-- This data-structure represents a min-heap with the root node *removed*.
+-- (internally the filling of root value and sifting down is delayed).
+--
+-- Also there isn't *insert* operation, i.e. the heap can only shrink.
+-- Other heap usual heap opeartions are *create-heap*, *extract-min* and *replace*.
+-- However, as the 'MutableHeap' always represents a heap with its root (minimum value)
+-- extracted, *extract-min* is "fused" to other operations.
+module KMerge.Heap (
+    MutableHeap,
+    newMutableHeap,
+    replaceRoot,
+    extract,
+) where
+
+import           Control.Monad.Primitive (PrimMonad (PrimState), RealWorld)
+import qualified Control.Monad.ST as Lazy
+import qualified Control.Monad.ST as Strict
+import           Data.Bits (unsafeShiftL, unsafeShiftR)
+import           Data.Foldable.WithIndex (ifor_)
+import           Data.Primitive (SmallMutableArray, newSmallArray,
+                     readSmallArray, writeSmallArray)
+import           Data.Primitive.PrimVar (PrimVar, newPrimVar, readPrimVar,
+                     writePrimVar)
+import           Unsafe.Coerce (unsafeCoerce)
+
+-- | Mutable heap for k-merge algorithm.
+data MutableHeap s a = MH
+    !(PrimVar s Int) -- ^ element count, size
+    !(SmallMutableArray s a)
+
+-- | Placeholder value used to fill the internal array.
+placeholder :: a
+placeholder = unsafeCoerce ()
+
+-- | Create new heap, and immediately extract its minimum value.
+newMutableHeap :: forall a m. (PrimMonad m, Ord a) => [a] -> m (MutableHeap (PrimState m) a, Maybe a)
+newMutableHeap xs = do
+    let !size = length xs
+
+    arr <- newSmallArray size placeholder
+    ifor_ xs $ \idx x -> do
+        writeSmallArray arr idx x
+        siftUp arr x idx
+
+    sizeRef <- newPrimVar size
+
+    if size <= 0
+    then return $! (MH sizeRef arr, Nothing)
+    else do
+        x <- readSmallArray arr 0
+        writeSmallArray arr 0 placeholder
+        return $! (MH sizeRef arr, Just x)
+
+-- | Replace the minimum-value, and immediately extract the new minimum value.
+replaceRoot :: forall a m. (PrimMonad m, Ord a) => MutableHeap (PrimState m) a -> a -> m a
+replaceRoot (MH sizeRef arr) val = do
+    size <- readPrimVar sizeRef
+    if size <= 1
+    then return val
+    else do
+        writeSmallArray arr 0 val
+        siftDown arr size val 0
+        x <- readSmallArray arr 0
+        return x
+
+{-# SPECIALIZE replaceRoot :: forall a.   Ord a => MutableHeap RealWorld a -> a -> IO          a #-}
+{-# SPECIALIZE replaceRoot :: forall a s. Ord a => MutableHeap s         a -> a -> Strict.ST s a #-}
+{-# SPECIALIZE replaceRoot :: forall a s. Ord a => MutableHeap s         a -> a -> Lazy.ST   s a #-}
+
+-- | Extract the next minimum value.
+extract :: forall a m. (PrimMonad m, Ord a) => MutableHeap (PrimState m) a -> m (Maybe a)
+extract (MH sizeRef arr) = do
+    size <- readPrimVar sizeRef
+    if size <= 1
+    then return Nothing
+    else do
+        writePrimVar sizeRef $! size - 1
+        val <- readSmallArray arr (size - 1)
+        writeSmallArray arr 0 val
+        siftDown arr size val 0
+        x <- readSmallArray arr 0
+        writeSmallArray arr (size - 1) placeholder
+        return $! Just x
+
+{-# SPECIALIZE extract :: forall a.   Ord a => MutableHeap RealWorld a -> IO          (Maybe a) #-}
+{-# SPECIALIZE extract :: forall a s. Ord a => MutableHeap s         a -> Strict.ST s (Maybe a) #-}
+{-# SPECIALIZE extract :: forall a s. Ord a => MutableHeap s         a -> Lazy.ST   s (Maybe a) #-}
+
+{-------------------------------------------------------------------------------
+  Internal operations
+-------------------------------------------------------------------------------}
+
+siftUp :: forall a m. (PrimMonad m, Ord a) => SmallMutableArray (PrimState m) a -> a -> Int -> m ()
+siftUp !arr !x = loop where
+    loop !idx
+        | idx <= 0
+        = return ()
+
+        | otherwise
+        = do
+            let !parent = halfOf (idx - 1)
+            p <- readSmallArray arr parent
+            if x < p
+            then do
+                writeSmallArray arr parent x
+                writeSmallArray arr idx    p
+                loop parent
+            else return ()
+
+{-# SPECIALIZE siftUp :: forall a.   Ord a => SmallMutableArray RealWorld a -> a -> Int -> IO          () #-}
+{-# SPECIALIZE siftUp :: forall a s. Ord a => SmallMutableArray s         a -> a -> Int -> Strict.ST s () #-}
+{-# SPECIALIZE siftUp :: forall a s. Ord a => SmallMutableArray s         a -> a -> Int -> Lazy.ST   s () #-}
+
+siftDown :: forall a m. (PrimMonad m, Ord a) => SmallMutableArray (PrimState m) a -> Int -> a -> Int -> m ()
+siftDown !arr !size !x = loop where
+    loop !idx
+        | rgt < size
+        = do
+            l <- readSmallArray arr lft
+            r <- readSmallArray arr rgt
+
+            if x <= l
+            then do
+                if x <= r
+                then return ()
+                else do
+                    -- r < x <= l; swap x and r
+                    writeSmallArray arr rgt x
+                    writeSmallArray arr idx r
+                    loop rgt
+            else do
+                if l <= r
+                then do
+                    -- l < x, l <= r; swap x and l
+                    writeSmallArray arr idx l
+                    writeSmallArray arr lft x
+                    loop lft
+                else do
+                    -- r < l <= x; swap x and r
+                    writeSmallArray arr rgt x
+                    writeSmallArray arr idx r
+                    loop rgt
+
+        -- there's only left value
+        | lft < size
+        = do
+            l <- readSmallArray arr lft
+            if x <= l
+            then return ()
+            else do
+                writeSmallArray arr idx l
+                writeSmallArray arr lft x
+                -- there is no need to loop further, lft was the last value.
+
+        | otherwise
+        = return ()
+      where
+        !lft = doubleOf idx + 1
+        !rgt = doubleOf idx + 2
+
+{-# SPECIALIZE siftDown :: forall a.   Ord a => SmallMutableArray RealWorld a -> Int -> a -> Int -> IO          () #-}
+{-# SPECIALIZE siftDown :: forall a s. Ord a => SmallMutableArray s         a -> Int -> a -> Int -> Strict.ST s () #-}
+{-# SPECIALIZE siftDown :: forall a s. Ord a => SmallMutableArray s         a -> Int -> a -> Int -> Lazy.ST   s () #-}
+
+{-------------------------------------------------------------------------------
+  Helpers
+-------------------------------------------------------------------------------}
+
+halfOf :: Int -> Int
+halfOf i = unsafeShiftR i 1
+{-# INLINE halfOf #-}
+
+doubleOf :: Int -> Int
+doubleOf i = unsafeShiftL i 1
+{-# INLINE doubleOf #-}
---
+++ src-kmerge/KMerge/LoserTree.hs
@@ -0,0 +1,201 @@
+{-# LANGUAGE BangPatterns        #-}
+{-# LANGUAGE CPP                 #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fexpose-all-unfoldings #-}
+module KMerge.LoserTree (
+    MutableLoserTree,
+    newLoserTree,
+    replace,
+    remove,
+) where
+
+import           Control.Monad.Primitive (PrimMonad (PrimState), RealWorld)
+import qualified Control.Monad.ST as Lazy
+import qualified Control.Monad.ST as Strict
+import           Data.Bits (unsafeShiftR)
+import           Data.Primitive (MutablePrimArray, SmallMutableArray,
+                     newPrimArray, newSmallArray, readPrimArray, readSmallArray,
+                     setPrimArray, writePrimArray, writeSmallArray)
+import           Data.Primitive.PrimVar (PrimVar, newPrimVar, readPrimVar,
+                     writePrimVar)
+import           Unsafe.Coerce (unsafeCoerce)
+
+-- | Mutable Loser Tree.
+data MutableLoserTree s a = MLT
+    !(PrimVar s Int)                 -- ^ element count, i.e. size.
+    !(PrimVar s Int)                 -- ^ index of the hole (i.e. winner's initial index)
+    !(MutablePrimArray s Int)        -- ^ indices, we store the index of first match. -1 if there is no match.
+    !(SmallMutableArray s a)         -- ^ values
+
+placeholder :: a
+placeholder = unsafeCoerce ()
+
+-- | Create new 'MutableLoserTree'.
+--
+-- The second half of a pair is the winner value (only losers are stored in the tree).
+--
+newLoserTree :: forall a m. (PrimMonad m, Ord a) => [a] -> m (MutableLoserTree (PrimState m) a, Maybe a)
+newLoserTree [] = do
+    ids <- newPrimArray 0
+    arr <- newSmallArray 0 placeholder
+    sizeRef <- newPrimVar 0
+    holeRef <- newPrimVar 0
+    return $! (MLT sizeRef holeRef ids arr, Nothing)
+newLoserTree [x] = do
+    ids <- newPrimArray 0
+    arr <- newSmallArray 0 placeholder
+    sizeRef <- newPrimVar 0
+    holeRef <- newPrimVar 0
+    return $! (MLT sizeRef holeRef ids arr, Just x)
+newLoserTree xs0 = do
+    -- allocate array, we need one less than there are elements.
+    -- one of the elements will be the winner.
+    ids <- newPrimArray  (len - 1)
+    setPrimArray ids 0 (len - 1) (-1)
+    arr <- newSmallArray (len - 1) placeholder
+
+    loop ids arr (len - 1) xs0
+  where
+    !len = length xs0
+
+    loop :: MutablePrimArray (PrimState m) Int -> SmallMutableArray (PrimState m) a -> Int -> [a] -> m (MutableLoserTree (PrimState m) a, Maybe a)
+    loop !_   !_   !_   []     = error "should not happen"
+    loop  ids  arr  idx (x:xs) = do
+        sift ids arr (parentOf idx) (parentOf idx) x idx xs
+
+    sift :: MutablePrimArray (PrimState m) Int -> SmallMutableArray (PrimState m) a -> Int -> Int -> a -> Int -> [a] -> m (MutableLoserTree (PrimState m) a, Maybe a)
+    sift !ids !arr !idxX !j !x !idx0 xs = do
+        !idxY <- readPrimArray ids j
+        y     <- readSmallArray arr j
+        if idxY < 0
+        then do
+            writePrimArray  ids j idxX
+            writeSmallArray arr j x
+            loop ids arr (idx0 + 1) xs
+        else if j <= 0
+        then do
+                if x <= y
+                then do
+                    sizeRef <- newPrimVar (len - 1)
+                    holeRef <- newPrimVar idxX
+                    return (MLT sizeRef holeRef ids arr, Just x)
+                else do
+                    writePrimArray  ids j idxX
+                    writeSmallArray arr j x
+                    sizeRef <- newPrimVar (len - 1)
+                    holeRef <- newPrimVar idxY
+                    return (MLT sizeRef holeRef ids arr, Just y)
+        else do
+                if x < y
+                then do
+                    sift ids arr idxX (parentOf j) x idx0 xs
+                else do
+                    writePrimArray  ids j idxX
+                    writeSmallArray arr j x
+                    sift ids arr idxY (parentOf j) y idx0 xs
+
+{-# SPECIALIZE newLoserTree :: forall a.   Ord a => [a] -> IO          (MutableLoserTree RealWorld a, Maybe a) #-}
+{-# SPECIALIZE newLoserTree :: forall a s. Ord a => [a] -> Strict.ST s (MutableLoserTree s         a, Maybe a) #-}
+{-# SPECIALIZE newLoserTree :: forall a s. Ord a => [a] -> Lazy.ST   s (MutableLoserTree s         a, Maybe a) #-}
+
+{-------------------------------------------------------------------------------
+  Updates
+-------------------------------------------------------------------------------}
+
+{-# SPECIALIZE replace :: forall a.   Ord a => MutableLoserTree RealWorld a -> a -> IO          a #-}
+{-# SPECIALIZE replace :: forall a s. Ord a => MutableLoserTree s         a -> a -> Strict.ST s a #-}
+{-# SPECIALIZE replace :: forall a s. Ord a => MutableLoserTree s         a -> a -> Lazy.ST s   a #-}
+
+{-# SPECIALIZE remove :: forall a.   Ord a => MutableLoserTree RealWorld a -> IO          (Maybe a) #-}
+{-# SPECIALIZE remove :: forall a s. Ord a => MutableLoserTree s         a -> Strict.ST s (Maybe a) #-}
+{-# SPECIALIZE remove :: forall a s. Ord a => MutableLoserTree s         a -> Lazy.ST s   (Maybe a) #-}
+
+-- | Don't fill the winner "hole". Return a next winner of (smaller) tournament.
+remove :: forall a m. (PrimMonad m, Ord a) => MutableLoserTree (PrimState m) a -> m (Maybe a)
+remove (MLT sizeRef holeRef ids arr) = do
+    size <- readPrimVar sizeRef
+    if size <= 0
+    then return Nothing
+    else do
+        writePrimVar sizeRef (size - 1)
+        hole <- readPrimVar holeRef
+        siftEmpty hole
+  where
+    siftEmpty :: Int -> m (Maybe a)
+    siftEmpty !j = do
+        !idxY <- readPrimArray ids j
+        y     <- readSmallArray arr j
+        if j <= 0
+        then if idxY < 0
+            then return Nothing
+            else do
+                writePrimArray  ids j (-1)
+                writeSmallArray arr j placeholder
+                writePrimVar holeRef idxY
+                return (Just y)
+        else if idxY < 0
+            then
+                siftEmpty (parentOf j)
+            else do
+                writePrimArray  ids j (-1)
+                writeSmallArray arr j placeholder
+                Just <$> siftUp ids arr holeRef (parentOf j) idxY y
+
+-- | Fill the winner "hole" with a new element. Return a new tournament winner.
+replace :: forall a m. (PrimMonad m, Ord a) => MutableLoserTree (PrimState m) a -> a -> m a
+replace (MLT sizeRef holeRef ids arr) val = do
+    size <- readPrimVar sizeRef
+    if size <= 0
+    then return val
+    else do
+        hole <- readPrimVar holeRef
+        siftUp ids arr holeRef hole hole val
+
+{-# SPECIALIZE siftUp :: forall a.   Ord a => MutablePrimArray RealWorld Int -> SmallMutableArray RealWorld a -> PrimVar RealWorld Int -> Int -> Int -> a -> IO          a #-}
+{-# SPECIALIZE siftUp :: forall a s. Ord a => MutablePrimArray s Int         -> SmallMutableArray s         a -> PrimVar s         Int -> Int -> Int -> a -> Strict.ST s a #-}
+{-# SPECIALIZE siftUp :: forall a s. Ord a => MutablePrimArray s Int         -> SmallMutableArray s         a -> PrimVar s         Int -> Int -> Int -> a -> Lazy.ST s   a #-}
+
+siftUp :: forall a m. (PrimMonad m, Ord a) => MutablePrimArray (PrimState m) Int -> SmallMutableArray (PrimState m) a -> PrimVar (PrimState m) Int -> Int -> Int -> a -> m a
+siftUp ids arr holeRef = sift
+  where
+    sift :: Int -> Int -> a -> m a
+    sift !j !idxX !x = do
+        !idxY <- readPrimArray ids j
+        y     <- readSmallArray arr j
+        if j <= 0
+        then if idxY < 0
+            then do
+                writePrimVar holeRef idxX
+                return x
+            else do
+                if x <= y
+                then do
+                    writePrimVar holeRef idxX
+                    return x
+                else do
+                    writePrimArray  ids j idxX
+                    writeSmallArray arr j x
+                    writePrimVar holeRef idxY
+                    return y
+        else if idxY < 0
+            then sift (parentOf j) idxX x
+            else do
+                if x <= y
+                then do
+                    sift (parentOf j) idxX x
+                else do
+                    writePrimArray  ids j idxX
+                    writeSmallArray arr j x
+                    sift (parentOf j) idxY y
+
+{-------------------------------------------------------------------------------
+  Helpers
+-------------------------------------------------------------------------------}
+
+halfOf :: Int -> Int
+halfOf i = unsafeShiftR i 1
+{-# INLINE halfOf #-}
+
+parentOf :: Int -> Int
+parentOf i = halfOf (i - 1)
+{-# INLINE parentOf #-}
