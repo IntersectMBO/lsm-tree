fe16d5c587fcefcec90666c1371a92e3d2dbee9e
Duncan Coutts <duncan@well-typed.com>
2023-11-30 14:18:56 +0000

    Add a reference implementation and test for the page format
    Covers encoding/decoding and calculating the expected page size
    while accumuating key/operation pairs.
    
    Includes tests for the usual round trip properties, and checking size
    and offset calculations.
    
    This should be the basis for high performance (de)serialisation and
    intra-page lookup implementations.

--- prototypes/Test.hs
+++ prototypes/Test.hs
@@ -2,9 +2,11 @@ module Main (main) where
 
 import           Test.Tasty
 
+import qualified FormatPage
 import qualified ScheduledMergesTestQLS
 
 main :: IO ()
 main = defaultMain $ testGroup "prototype" [
-      ScheduledMergesTestQLS.tests
+      ScheduledMergesTestQLS.tests,
+      FormatPage.tests
     ]
