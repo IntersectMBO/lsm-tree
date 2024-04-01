6b377959f9ed2640adfeaef0613e085700f3ac67
Duncan Coutts <duncan@well-typed.com>
2024-04-01 16:59:29 +0100

    Support base 4.16 (ghc-9.2) and earlier.
    The sameByteArray# primop was only added recently.

--- src/Database/LSMTree/Internal/Assertions.hs
+++ src/Database/LSMTree/Internal/Assertions.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP       #-}
 {-# LANGUAGE MagicHash #-}
 module Database.LSMTree.Internal.Assertions (
     assert,
@@ -7,7 +8,12 @@ module Database.LSMTree.Internal.Assertions (
 
 import           Control.Exception (assert)
 import           Data.Primitive.ByteArray (ByteArray (..), sizeofByteArray)
+#if MIN_VERSION_base(4,17,0)
 import           GHC.Exts (isTrue#, sameByteArray#)
+#else
+import           GHC.Exts (ByteArray#, MutableByteArray#, isTrue#,
+                     sameMutableByteArray#, unsafeCoerce#)
+#endif
 
 isValidSlice :: Int -> Int -> ByteArray -> Bool
 isValidSlice off len ba =
@@ -17,5 +23,13 @@ isValidSlice off len ba =
     (off + len) <= sizeofByteArray ba
 
 sameByteArray :: ByteArray -> ByteArray -> Bool
-sameByteArray (ByteArray ba1) (ByteArray ba2) =
-    isTrue# (sameByteArray# ba1 ba2)
+sameByteArray (ByteArray ba1#) (ByteArray ba2#) =
+#if MIN_VERSION_base(4,17,0)
+    isTrue# (sameByteArray# ba1# ba2#)
+#else
+    isTrue# (sameMutableByteArray# (unsafeCoerceByteArray# ba1#)
+                                   (unsafeCoerceByteArray# ba2#))
+  where
+    unsafeCoerceByteArray# :: ByteArray# -> MutableByteArray# s
+    unsafeCoerceByteArray# = unsafeCoerce#
+#endif
