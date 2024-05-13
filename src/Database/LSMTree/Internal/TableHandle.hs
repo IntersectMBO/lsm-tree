eaec2ac09a19f5ec7cfc5292d7fc44b951ac3e23
Joris Dral <joris@well-typed.com>
2024-05-13 10:43:45 +0200

    Representation of sessions, table handles, and configuration options

---
+++ src/Database/LSMTree/Internal/TableHandle.hs
@@ -0,0 +1,176 @@
+-- TODO: remove once the top-level bindings are in use
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+
+module Database.LSMTree.Internal.TableHandle (
+    -- * Session
+    Session
+    -- * Table handle
+  , TableHandle
+    -- * configuration
+  , TableConfig (..)
+  , ResolveMupsert (..)
+  , SizeRatio (..)
+  , MergePolicy (..)
+  , BloomFilterAlloc (..)
+  ) where
+
+import           Control.Concurrent.Class.MonadMVar.Strict (StrictMVar)
+import           Data.BloomFilter (Bloom)
+import qualified Data.Vector as V
+import           Data.Word (Word32)
+import           Database.LSMTree.Internal.IndexCompact (IndexCompact)
+import           Database.LSMTree.Internal.Run (Run)
+import qualified Database.LSMTree.Internal.Run as Run
+import           Database.LSMTree.Internal.Serialise (SerialisedKey,
+                     SerialisedValue)
+import           Database.LSMTree.Internal.WriteBuffer (WriteBuffer)
+import           System.FS.API (FsPath, HasFS)
+import           System.FS.BlockIO.API (HasBlockIO)
+
+{-------------------------------------------------------------------------------
+  Session
+-------------------------------------------------------------------------------}
+
+-- | A session provides context that is shared across multiple table handles.
+--
+-- For more information, see 'Database.LSMTree.Common.Session'.
+data Session m h = Session {
+    -- | The path to the directory in which this sesion is live. This is a path
+    -- relative to root of the 'HasFS' instance.
+    sessionRoot        :: !FsPath
+  , sessionHasFS       :: !(HasFS m h)
+  , sessionHasBlockIO  :: !(HasBlockIO m h)
+    -- | A session-wide shared, atomic counter that is used to produce unique
+    -- names for run files.
+  , sessionUniqCounter :: !(StrictMVar m Integer)
+  }
+
+{-------------------------------------------------------------------------------
+  Table handle
+-------------------------------------------------------------------------------}
+
+-- | A handle to an on-disk key\/value table.
+--
+-- For more information, see 'Database.LSMTree.Normal.TableHandle'.
+--
+-- NOTE: a table handle is __not__ thread-safe.
+data TableHandle m h = TableHandle {
+    tableSession     :: !(Session m h)
+  , tableConfig      :: !TableConfig
+  , tableWriteBuffer :: !(StrictMVar m (WriteBuffer))
+    -- | A hierarchy of levels. The vector indexes double as level numbers.
+  , tableLevels      :: !(StrictMVar m (V.Vector (Level h)))
+    -- | Cache of flattened 'levels'.
+    --
+    -- INVARIANT: when 'level's is modified, this cache should be updated as
+    -- well, for example using 'mkLevelsCache'.
+  , tableLevelsCache :: !(StrictMVar m (LevelsCache h))
+  }
+
+-- | Runs in order from newer to older
+newtype Level h = Level {
+    residentRuns :: V.Vector (Run h)
+{- TODO: this is where ongoing merges appear once we implement scheduling ,
+  incomingRuns :: MergingRun
+-}
+  }
+
+-- | Flattend cache of the runs that referenced by a table handle.
+--
+-- This cache includes a vector of runs, but also vectors of the runs broken
+-- down into components, like bloom filters, fence pointer indexes and file
+-- handles. This allows for quick access in the lookup code. Recomputing this
+-- cache should be relatively rare.
+--
+-- Use 'mkLevelsCache' to ensure that there are no mismatches between the vector
+-- of runs and the vectors of run components.
+data LevelsCache h = LevelsCache_ {
+    cachedRuns      :: !(V.Vector (Run h))
+  , cachedFilters   :: !(V.Vector (Bloom SerialisedKey))
+  , cachedIndexes   :: !(V.Vector IndexCompact)
+  , cachedKOpsFiles :: !(V.Vector h)
+  }
+
+-- | Flatten the argument 'Level's into a single vector of runs, and use that to
+-- populate the 'LevelsCache'.
+mkLevelsCache :: V.Vector (Level h) -> LevelsCache h
+mkLevelsCache lvls = LevelsCache_ {
+      cachedRuns      = rs
+    , cachedFilters   = V.map Run.runFilter rs
+    , cachedIndexes   = V.map Run.runIndex rs
+    , cachedKOpsFiles = V.map Run.runKOpsFile rs
+    }
+  where rs = V.concatMap residentRuns lvls
+
+{-------------------------------------------------------------------------------
+  Configuration
+-------------------------------------------------------------------------------}
+
+-- | Table configuration parameters, including LSM tree tuning parameters.
+--
+-- Some config options are fixed (for now):
+--
+-- * Merge policy: Tiering
+--
+-- * Size ratio: 4
+data TableConfig = TableConfig {
+    confMergePolicy      :: !MergePolicy
+    -- Size ratio between the capacities of adjacent levels.
+  , confSizeRatio        :: !SizeRatio
+    -- | Total number of bytes that the write buffer can use.
+    --
+    -- The maximum is 4GiB, which should be more than enough for realistic
+    -- applications.
+  , confWriteBufferAlloc :: !Word32
+  , confBloomFilterAlloc :: !BloomFilterAlloc
+    -- | Function for resolving 'Mupsert' values. This should be 'Nothing' for
+    -- normal tables.
+  , confResolveMupsert   :: !(Maybe ResolveMupsert)
+  }
+  deriving Show
+
+data MergePolicy =
+    -- | Use tiering on intermediate levels, and levelling on the last level.
+    -- This makes it easier for delete operations to disappear on the last
+    -- level.
+    MergePolicyLazyLevelling
+{- TODO: disabled for now. Would we ever want to provide pure tiering?
+  | MergePolicyTiering
+-}
+{- TODO: disabled for now
+  | MergePolicyLevelling
+-}
+  deriving (Show, Eq)
+
+data SizeRatio = Four
+  deriving (Show, Eq)
+
+-- | Allocation method for bloom filters.
+--
+-- NOTE: a __physical__ database entry is a key\/operation pair that exists in a
+-- file, i.e., a run. Multiple physical entries that have the same key
+-- constitute a __logical__ database entry.
+data BloomFilterAlloc =
+    -- | Allocate a fixed number of bits per physical entry in each bloom
+    -- filter.
+    AllocFixed
+      Word32 -- ^ Bits per physical entry.
+  | -- | Allocate as many bits as required per physical entry to get the requested
+    -- false-positive rate. Do this for each bloom filter.
+    AllocRequestFPR
+      Double -- ^ Requested FPR.
+{- TODO: disabled for now
+  | -- | Allocate bits amongst all bloom filters according to the Monkey algorithm.
+    --
+    -- TODO: add more configuration paramters that the Monkey algorithm might
+    -- need once we implement it.
+    AllocMonkey
+      Word32 -- ^ Total number of bytes that bloom filters can use collectively.
+             --
+             -- The maximum is 4GiB, which should be more than enough for
+             -- realistic applications.
+ -}
+  deriving (Show, Eq)
+
+newtype ResolveMupsert = ResolveMupsert (SerialisedValue -> SerialisedValue -> SerialisedValue)
+instance Show ResolveMupsert where show _ = "ResolveMupsert function can not be printed"
