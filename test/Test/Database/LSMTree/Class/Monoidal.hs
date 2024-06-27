cc199eccd804db6d836a4867e372b4b0c87a1f1f
Joris Dral <joris@well-typed.com>
2024-06-27 13:44:01 +0200

    Use `bracket`-style functions in `Class` tests

--- test/Test/Database/LSMTree/Class/Monoidal.hs
+++ test/Test/Database/LSMTree/Class/Monoidal.hs
@@ -9,11 +9,13 @@ import           Data.List (sortOn)
 import           Data.Maybe (fromMaybe)
 import           Data.Proxy (Proxy (..))
 import           Data.Word (Word64)
-import           Database.LSMTree.Class.Monoidal
+import           Database.LSMTree.Class.Monoidal hiding (withTableDuplicate,
+                     withTableNew, withTableOpen)
+import qualified Database.LSMTree.Class.Monoidal as Class
 import           Database.LSMTree.Common (SomeUpdateConstraint (..),
                      mkSnapshotName)
 import           Database.LSMTree.Extras.Generators ()
-import           Database.LSMTree.ModelIO.Monoidal (LookupResult (..),
+import           Database.LSMTree.ModelIO.Monoidal (IOLike, LookupResult (..),
                      Range (..), RangeLookupResult (..), TableHandle,
                      Update (..))
 import           Test.Tasty (TestTree, testGroup)
@@ -46,14 +48,18 @@ tests = testGroup "Test.Database.LSMTree.Class.Monoidal"
 type Key = Word64
 type Value = BS.ByteString
 
-makeNewTable :: forall h. IsTableHandle h => Proxy h
-    -> [(Key, Update Value)]
-    -> IO (Session h IO, h IO Key Value)
-makeNewTable h ups = do
-    s <- openSession
-    hdl <- new @h s (testTableConfig h)
-    updates hdl ups
-    return (s, hdl)
+-- | create session, table handle, and populate it with some data.
+withTableNew ::
+     forall h m a. (IsTableHandle h, IOLike m)
+  => Proxy h
+  -> [(Key, Update Value)]
+  -> (Session h m -> h m Key Value -> m a)
+  -> m a
+withTableNew h ups action =
+    withSession  $ \sesh ->
+      Class.withTableNew @h sesh (testTableConfig h) $ \table -> do
+        updates table ups
+        action sesh table
 
 -------------------------------------------------------------------------------
 -- implement classic QC tests for basic k/v properties
@@ -65,14 +71,12 @@ prop_lookupInsert ::
   => Proxy h -> [(Key, Update Value)]
   -> Key  -> Value -> Property
 prop_lookupInsert h ups k v = ioProperty $ do
-    -- create session, table handle, and populate it with some data.
-    (_, hdl) <- makeNewTable h ups
+    withTableNew h ups $ \_ hdl -> do
+      -- the main dish
+      inserts hdl [(k, v)]
+      res <- lookups hdl [k]
 
-    -- the main dish
-    inserts hdl [(k, v)]
-    res <- lookups hdl [k]
-
-    return $ res === [Found k v]
+      return $ res === [Found k v]
 
 -- | Insert doesn't change the lookup results of other keys.
 prop_lookupInsertElse ::
@@ -80,15 +84,14 @@ prop_lookupInsertElse ::
   => Proxy h -> [(Key, Update Value)]
   -> Key  -> Value -> [Key] -> Property
 prop_lookupInsertElse h ups k v testKeys = ioProperty $ do
-    -- create session, table handle, and populate it with some data.
-    (_, hdl) <- makeNewTable h ups
+    withTableNew h ups $ \_ hdl -> do
 
-    let testKeys' = filter (/= k) testKeys
-    res1 <- lookups hdl testKeys'
-    inserts hdl [(k, v)]
-    res2 <-  lookups hdl testKeys'
+      let testKeys' = filter (/= k) testKeys
+      res1 <- lookups hdl testKeys'
+      inserts hdl [(k, v)]
+      res2 <-  lookups hdl testKeys'
 
-    return $ res1 === res2
+      return $ res1 === res2
 
 -- | You cannot lookup what you have just deleted
 prop_lookupDelete ::
@@ -96,10 +99,10 @@ prop_lookupDelete ::
   => Proxy h -> [(Key, Update Value)]
   -> Key -> Property
 prop_lookupDelete h ups k = ioProperty $ do
-    (_, hdl) <- makeNewTable h ups
-    deletes hdl [k]
-    res <- lookups hdl [k]
-    return $ res === [NotFound k]
+    withTableNew h ups $ \_ hdl -> do
+      deletes hdl [k]
+      res <- lookups hdl [k]
+      return $ res === [NotFound k]
 
 -- | Delete doesn't change the lookup results of other keys
 prop_lookupDeleteElse ::
@@ -107,15 +110,14 @@ prop_lookupDeleteElse ::
   => Proxy h -> [(Key, Update Value)]
   -> Key  -> [Key] -> Property
 prop_lookupDeleteElse h ups k testKeys = ioProperty $ do
-    -- create session, table handle, and populate it with some data.
-    (_, hdl) <- makeNewTable h ups
+    withTableNew h ups $ \_ hdl -> do
 
-    let testKeys' = filter (/= k) testKeys
-    res1 <- lookups hdl testKeys'
-    deletes hdl [k]
-    res2 <-  lookups hdl testKeys'
+      let testKeys' = filter (/= k) testKeys
+      res1 <- lookups hdl testKeys'
+      deletes hdl [k]
+      res2 <-  lookups hdl testKeys'
 
-    return $ res1 === res2
+      return $ res1 === res2
 
 -- | Last insert wins.
 prop_insertInsert ::
@@ -123,10 +125,10 @@ prop_insertInsert ::
   => Proxy h -> [(Key, Update Value)]
   -> Key -> Value -> Value -> Property
 prop_insertInsert h ups k v1 v2 = ioProperty $ do
-    (_, hdl) <- makeNewTable h ups
-    inserts hdl [(k, v1), (k, v2)]
-    res <- lookups hdl [k]
-    return $ res === [Found k v2]
+    withTableNew h ups $ \_ hdl -> do
+      inserts hdl [(k, v1), (k, v2)]
+      res <- lookups hdl [k]
+      return $ res === [Found k v2]
 
 -- | Inserts with different keys don't interfere.
 prop_insertCommutes ::
@@ -134,11 +136,11 @@ prop_insertCommutes ::
     => Proxy h -> [(Key, Update Value)]
     -> Key -> Value -> Key -> Value -> Property
 prop_insertCommutes h ups k1 v1 k2 v2 = k1 /= k2 ==> ioProperty do
-    (_, hdl) <- makeNewTable h ups
-    inserts hdl [(k1, v1), (k2, v2)]
+    withTableNew h ups $ \_ hdl -> do
+      inserts hdl [(k1, v1), (k2, v2)]
 
-    res <- lookups hdl [k1,k2]
-    return $ res === [Found k1 v1, Found k2 v2]
+      res <- lookups hdl [k1,k2]
+      return $ res === [Found k1 v1, Found k2 v2]
 
 -------------------------------------------------------------------------------
 -- implement classic QC tests for range lookups
@@ -157,20 +159,20 @@ prop_insertLookupRange ::
   => Proxy h -> [(Key, Update Value)]
   -> Key -> Value -> Range Key  -> Property
 prop_insertLookupRange h ups k v r = ioProperty $ do
-    (_, hdl) <- makeNewTable h ups
+    withTableNew h ups $ \_ hdl -> do
 
-    res <- rangeLookup hdl r
+      res <- rangeLookup hdl r
 
-    inserts hdl [(k, v)]
+      inserts hdl [(k, v)]
 
-    res' <- rangeLookup hdl r
+      res' <- rangeLookup hdl r
 
-    let p :: RangeLookupResult Key Value -> Bool
-        p rlr = rangeLookupResultKey rlr /= k
+      let p :: RangeLookupResult Key Value -> Bool
+          p rlr = rangeLookupResultKey rlr /= k
 
-    if evalRange r k
-    then return $ sortOn rangeLookupResultKey (FoundInRange k v : filter p res) === res'
-    else return $ res === res'
+      if evalRange r k
+      then return $ sortOn rangeLookupResultKey (FoundInRange k v : filter p res) === res'
+      else return $ res === res'
 
 -------------------------------------------------------------------------------
 -- implement classic QC tests for split-value BLOB retrieval
@@ -188,16 +190,15 @@ prop_lookupUpdate ::
   => Proxy h -> [(Key, Update Value)]
   -> Key  -> Value -> Value -> Property
 prop_lookupUpdate h ups k v1 v2 = ioProperty $ do
-    -- create session, table handle, and populate it with some data.
-    (_, hdl) <- makeNewTable h ups
+    withTableNew h ups $ \_ hdl -> do
 
-    -- the main dish
-    inserts hdl [(k, v1)]
-    mupserts hdl [(k, v2)]
-    res <- lookups hdl [k]
+      -- the main dish
+      inserts hdl [(k, v1)]
+      mupserts hdl [(k, v2)]
+      res <- lookups hdl [k]
 
-    -- notice the order.
-    return $ res === [Found k $ mergeU v2 v1]
+      -- notice the order.
+      return $ res === [Found k $ mergeU v2 v1]
 
 -------------------------------------------------------------------------------
 -- implement classic QC tests for monoidal table merges
@@ -208,27 +209,25 @@ prop_merge :: forall h.
   => Proxy h -> [(Key, Update Value)] -> [(Key, Update Value)]
   -> [Key] -> Property
 prop_merge h ups1 ups2 testKeys = ioProperty $ do
-    -- create two tables, from ups1 and ups2
-    (s, hdl1) <- makeNewTable h ups1
+    withTableNew h ups1 $ \s hdl1 -> do
+      Class.withTableNew  s (testTableConfig h) $ \hdl2 -> do
+        updates hdl2 ups2
 
-    hdl2 <- new @h s (testTableConfig h)
-    updates hdl2 ups2
+        -- merge them.
+        Class.withTableMerge hdl1 hdl2 $ \hdl3 -> do
 
-    -- merge them.
-    hdl3 <- merge hdl1 hdl2
+          -- results in parts and the merge table
+          res1 <- lookups hdl1 testKeys
+          res2 <- lookups hdl2 testKeys
+          res3 <- lookups hdl3 testKeys
 
-    -- results in parts and the merge table
-    res1 <- lookups hdl1 testKeys
-    res2 <- lookups hdl2 testKeys
-    res3 <- lookups hdl3 testKeys
+          let mergeResult :: LookupResult Key Value -> LookupResult Key Value -> LookupResult Key Value
+              mergeResult r@(NotFound _)   (NotFound _) = r
+              mergeResult   (NotFound _) r@(Found _ _)  = r
+              mergeResult r@(Found _ _)    (NotFound _) = r
+              mergeResult   (Found k v1)   (Found _ v2) = Found k (mergeU v1 v2)
 
-    let mergeResult :: LookupResult Key Value -> LookupResult Key Value -> LookupResult Key Value
-        mergeResult r@(NotFound _)   (NotFound _) = r
-        mergeResult   (NotFound _) r@(Found _ _)  = r
-        mergeResult r@(Found _ _)    (NotFound _) = r
-        mergeResult   (Found k v1)   (Found _ v2) = Found k (mergeU v1 v2)
-
-    return $ zipWith mergeResult res1 res2  == res3
+          return $ zipWith mergeResult res1 res2  == res3
 
 -------------------------------------------------------------------------------
 -- implement classic QC tests for snapshots
@@ -240,20 +239,20 @@ prop_snapshotNoChanges :: forall h.
     => Proxy h -> [(Key, Update Value)]
     -> [(Key, Update Value)] -> [Key] -> Property
 prop_snapshotNoChanges h ups ups' testKeys = ioProperty $ do
-    (sess, hdl1) <- makeNewTable h ups
+    withTableNew h ups $ \sess hdl1 -> do
 
-    res <- lookups hdl1 testKeys
+      res <- lookups hdl1 testKeys
 
-    let name = fromMaybe (error "invalid name") $ mkSnapshotName "foo"
+      let name = fromMaybe (error "invalid name") $ mkSnapshotName "foo"
 
-    snapshot name hdl1
-    updates hdl1 ups'
+      snapshot name hdl1
+      updates hdl1 ups'
 
-    hdl2 <- open @h sess name
+      Class.withTableOpen @h sess name $ \hdl2 -> do
 
-    res' <- lookups hdl2 testKeys
+        res' <- lookups hdl2 testKeys
 
-    return $ res == res'
+        return $ res == res'
 
 -- same snapshot may be opened multiple times,
 -- and the handles are separate.
@@ -262,18 +261,18 @@ prop_snapshotNoChanges2 :: forall h.
     => Proxy h -> [(Key, Update Value)]
     -> [(Key, Update Value)] -> [Key] -> Property
 prop_snapshotNoChanges2 h ups ups' testKeys = ioProperty $ do
-    (sess, hdl0) <- makeNewTable h ups
-    let name = fromMaybe (error "invalid name") $ mkSnapshotName "foo"
-    snapshot name hdl0
+    withTableNew h ups $ \sess hdl0 -> do
+      let name = fromMaybe (error "invalid name") $ mkSnapshotName "foo"
+      snapshot name hdl0
 
-    hdl1 <- open @h sess name
-    hdl2 <- open @h sess name
+      Class.withTableOpen @h sess name $ \hdl1 -> do
+        Class.withTableOpen @h sess name $ \hdl2 -> do
 
-    res <- lookups hdl1 testKeys
-    updates hdl1 ups'
-    res' <- lookups hdl2 testKeys
+          res <- lookups hdl1 testKeys
+          updates hdl1 ups'
+          res' <- lookups hdl2 testKeys
 
-    return $ res == res'
+          return $ res == res'
 
 -------------------------------------------------------------------------------
 -- implement classic QC tests for multiple writable table handles
@@ -288,15 +287,15 @@ prop_dupInsertInsert ::
   => Proxy h -> [(Key, Update Value)]
   -> Key -> Value -> Value -> [Key] -> Property
 prop_dupInsertInsert h ups k v1 v2 testKeys = ioProperty $ do
-    (_, hdl1) <- makeNewTable h ups
-    hdl2 <- duplicate hdl1
+    withTableNew h ups $ \_ hdl1 -> do
+      Class.withTableDuplicate hdl1 $ \hdl2 -> do
 
-    inserts hdl1 [(k, v1), (k, v2)]
-    inserts hdl2 [(k, v2)]
+        inserts hdl1 [(k, v1), (k, v2)]
+        inserts hdl2 [(k, v2)]
 
-    res1 <- lookups hdl1 testKeys
-    res2 <- lookups hdl2 testKeys
-    return $ res1 === res2
+        res1 <- lookups hdl1 testKeys
+        res2 <- lookups hdl2 testKeys
+        return $ res1 === res2
 
 -- | Different key inserts commute.
 prop_dupInsertCommutes ::
@@ -304,15 +303,15 @@ prop_dupInsertCommutes ::
     => Proxy h -> [(Key, Update Value)]
     -> Key -> Value -> Key -> Value -> [Key] -> Property
 prop_dupInsertCommutes h ups k1 v1 k2 v2 testKeys = k1 /= k2 ==> ioProperty do
-    (_, hdl1) <- makeNewTable h ups
-    hdl2 <- duplicate hdl1
+    withTableNew h ups $ \_ hdl1 -> do
+      Class.withTableDuplicate hdl1 $ \hdl2 -> do
 
-    inserts hdl1 [(k1, v1), (k2, v2)]
-    inserts hdl2 [(k2, v2), (k1, v1)]
+        inserts hdl1 [(k1, v1), (k2, v2)]
+        inserts hdl2 [(k2, v2), (k1, v1)]
 
-    res1 <- lookups hdl1 testKeys
-    res2 <- lookups hdl2 testKeys
-    return $ res1 === res2
+        res1 <- lookups hdl1 testKeys
+        res2 <- lookups hdl2 testKeys
+        return $ res1 === res2
 
 -- changes to one handle should not cause any visible changes in any others
 prop_dupNoChanges ::
@@ -320,15 +319,14 @@ prop_dupNoChanges ::
     => Proxy h -> [(Key, Update Value)]
     -> [(Key, Update Value)] -> [Key] -> Property
 prop_dupNoChanges h ups ups' testKeys = ioProperty $ do
-    (_, hdl1) <- makeNewTable h ups
+    withTableNew h ups $ \_ hdl1 -> do
 
-    res <- lookups hdl1 testKeys
+      res <- lookups hdl1 testKeys
 
-    hdl2 <- duplicate hdl1
-    updates hdl2 ups'
+      Class.withTableDuplicate hdl1 $ \hdl2 -> do
+        updates hdl2 ups'
 
-    -- lookup hdl1 again.
-    res' <- lookups hdl1 testKeys
+        -- lookup hdl1 again.
+        res' <- lookups hdl1 testKeys
 
-    return $ res == res'
-
+        return $ res == res'
