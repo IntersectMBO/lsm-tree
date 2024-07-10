043711c42a40ce3949560e2523be79fcbacb94d3
Oleg Grenrus <oleg.grenrus@iki.fi>
2024-07-10 15:35:12 +0300

    Change arena unsafe freeze test
    The unsafe test didn't use offset information, that will change soon.

--- test/Test/Data/Arena.hs
+++ test/Test/Data/Arena.hs
@@ -7,6 +7,7 @@ import           Control.Monad.ST (runST)
 import           Data.Arena
 import           Data.Primitive.ByteArray
 import           Data.Word (Word8)
+import           GHC.Exts (toList)
 import           Test.Tasty (TestTree, testGroup)
 import           Test.Tasty.HUnit (testCaseSteps, (@?=))
 
@@ -14,21 +15,22 @@ tests :: TestTree
 tests = testGroup "Test.Data.Arena"
     [ testCaseSteps "safe" $ \_info -> do
         let !ba = runST $ withUnmanagedArena $ \arena -> do
-                (off, mba) <- allocateFromArena arena 32 0
-                setByteArray mba off 32 (1 :: Word8)
-                freezeByteArray mba off 32
+                (off', mba) <- allocateFromArena arena 32 8
+                setByteArray mba off' 32 (1 :: Word8)
+                freezeByteArray mba off' 32
 
-        ba @?= byteArrayFromList (replicate 32 (1 :: Word8))
+        toList ba @?= replicate 32 (1 :: Word8)
 
-    , testCaseSteps "safe" $ \_info -> do
-        let !ba = runST $ withUnmanagedArena $ \arena -> do
-                (off, mba) <- allocateFromArena arena 32 0
-                setByteArray mba off 32 (1 :: Word8)
-                unsafeFreezeByteArray mba
+    , testCaseSteps "unsafe" $ \_info -> do
+        let !(off, ba) = runST $ withUnmanagedArena $ \arena -> do
+                (off', mba) <- allocateFromArena arena 32 8
+                setByteArray mba off' 32 (1 :: Word8)
+                ba' <- unsafeFreezeByteArray mba
+                return (off', ba')
 
 #if NO_IGNORE_ASSERTS
-        ba @?= byteArrayFromList (replicate 32 (0x77 :: Word8))
+        take 32 (drop off (toList ba)) @?= replicate 32 (0x77 :: Word8)
 #else
-        ba @?= byteArrayFromList (replicate 32 (1 :: Word8))
+        take 32 (drop off (toList ba)) @?= replicate 32 (1 :: Word8)
 #endif
     ]
