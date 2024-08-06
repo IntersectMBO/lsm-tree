4058f507c531e1a68f43d19ea3249613c2993e20
Duncan Coutts <duncan@well-typed.com>
2024-08-06 13:15:33 +0100

    Microoptimisation: use memset in bloom filter initialisation
    Using setByteArray at type Word8 allows an optimised impl using memset
    rather than using a loop at type Word64.

--- bloomfilter/src/Data/BloomFilter/BitVec64.hs
+++ bloomfilter/src/Data/BloomFilter/BitVec64.hs
@@ -62,7 +62,7 @@ new :: Word64 -> ST s (MBitVec64 s)
 new s
   | numWords >= 128 = do
     mba <- newPinnedByteArray numBytes
-    setByteArray mba 0 numWords (0 :: Word64)
+    setByteArray mba 0 numBytes (0 :: Word8)
     return (MBV64 (P.MVector 0 numWords mba))
   | otherwise =
     MBV64 <$> MP.new numWords
