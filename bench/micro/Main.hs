b0d105bf2a3eb139a7b06354f9947bdbcd0dbb94
Joris Dral <joris@well-typed.com>
2024-08-07 17:07:14 +0200

    Emit a warning when micro benchmarks are run in debug mode

--- bench/micro/Main.hs
+++ bench/micro/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE CPP #-}
+
 -- | Micro-benchmarks for the @lsm-tree@ library.
 module Main (main) where
 
@@ -12,13 +14,17 @@ import qualified Bench.Database.LSMTree.Monoidal
 import           Criterion.Main (defaultMain)
 
 main :: IO ()
-main = defaultMain [
-      Bench.Database.LSMTree.Internal.BloomFilter.benchmarks
-    , Bench.Database.LSMTree.Internal.IndexCompact.benchmarks
-    , Bench.Database.LSMTree.Internal.Lookup.benchmarks
-    , Bench.Database.LSMTree.Internal.Merge.benchmarks
-    , Bench.Database.LSMTree.Internal.RawPage.benchmarks
-    , Bench.Database.LSMTree.Internal.Serialise.benchmarks
-    , Bench.Database.LSMTree.Internal.WriteBuffer.benchmarks
-    , Bench.Database.LSMTree.Monoidal.benchmarks
-    ]
+main = do
+#ifdef NO_IGNORE_ASSERTS
+    putStrLn "WARNING: BENCHMARKING A BUILD IN DEBUG MODE"
+#endif
+    defaultMain [
+        Bench.Database.LSMTree.Internal.BloomFilter.benchmarks
+      , Bench.Database.LSMTree.Internal.IndexCompact.benchmarks
+      , Bench.Database.LSMTree.Internal.Lookup.benchmarks
+      , Bench.Database.LSMTree.Internal.Merge.benchmarks
+      , Bench.Database.LSMTree.Internal.RawPage.benchmarks
+      , Bench.Database.LSMTree.Internal.Serialise.benchmarks
+      , Bench.Database.LSMTree.Internal.WriteBuffer.benchmarks
+      , Bench.Database.LSMTree.Monoidal.benchmarks
+      ]
