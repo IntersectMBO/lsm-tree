af0beabee697eec45409e69177b99183428c4c67
Duncan Coutts <duncan@well-typed.com>
2024-09-24 13:12:46 +0100

    Refactor: push write buffer lookups into lookupsIO
    Previously lookupsIO dealt only with lookups in runs, and combining
    results from the write buffer was done in D.L.Internal. Originally that
    was ok because combining the write buffer results was simple, but it has
    grown more complex with the blobs feature being properly enabled.
    
    So now we pass it down into lookupsIO, specifically into
    intraPageLookups where all the results are combined. It works nicely
    here, since per-key results are accumulated into a mutable vector, so
    the only change is to initialise that array with the write buffer
    lookups. All the lookup results from runs are then accumulated on top.
    This should also mildly reduce allocations.
    
    In addition, instead of D.L.Internal.lookups getting passed in a
    function for converting into the final result, just return the
    intermediate representation and have the final conversion done in the
    API wrappers. This approach is more friendly for inlining and
    specialisation (so the deserialisation can be specialised at the call
    site).
    
    The benchmarks are updated but use an empty write buffer. Plausibly we
    might want some micro benchmarks for lookupIO et al that do make use of
    the write buffer too.

--- src/Database/LSMTree/Monoidal.hs
+++ src/Database/LSMTree/Monoidal.hs
@@ -208,6 +208,7 @@ close (Internal.MonoidalTable th) = Internal.close th
 -------------------------------------------------------------------------------}
 
 {-# SPECIALISE lookups :: (SerialiseKey k, SerialiseValue v, ResolveValue v) => V.Vector k -> TableHandle IO k v -> IO (V.Vector (LookupResult v)) #-}
+{-# INLINEABLE lookups #-}
 -- | Perform a batch of lookups.
 --
 -- Lookups can be performed concurrently from multiple Haskell threads.
@@ -217,21 +218,18 @@ lookups ::
   -> TableHandle m k v
   -> m (V.Vector (LookupResult v))
 lookups ks (Internal.MonoidalTable th) =
-    V.mapStrict (fmap Internal.deserialiseValue) <$!>
+    V.map toLookupResult <$>
     Internal.lookups
       (resolve @v Proxy)
       (V.map Internal.serialiseKey ks)
       th
-      toMonoidalLookupResult
-
-toMonoidalLookupResult :: Maybe (Entry.Entry v b) -> LookupResult v
-toMonoidalLookupResult = \case
-    Just e -> case e of
-      Entry.Insert v           -> Found v
-      Entry.InsertWithBlob _ _ -> error "toMonoidalLookupResult: InsertWithBlob unexpected"
-      Entry.Mupdate v          -> Found v
+  where
+    toLookupResult (Just e) = case e of
+      Entry.Insert v           -> Found (Internal.deserialiseValue v)
+      Entry.InsertWithBlob _ _ -> error "Monoidal.lookups: unexpected InsertWithBlob"
+      Entry.Mupdate v          -> Found (Internal.deserialiseValue v)
       Entry.Delete             -> NotFound
-    Nothing -> NotFound
+    toLookupResult Nothing = NotFound
 
 {-# SPECIALISE rangeLookup :: (SerialiseKey k, SerialiseValue v, ResolveValue v) => Range k -> TableHandle IO k v -> IO (V.Vector (QueryResult k v)) #-}
 -- | Perform a range lookup.
