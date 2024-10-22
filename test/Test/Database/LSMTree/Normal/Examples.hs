6cee94c3980d98b230434507998fedccbefa6211
Duncan Coutts <duncan@well-typed.com>
2024-10-22 11:37:24 +0100

    Restructure existing blobs unit test
    In preparation for adding more unit tests.

--- test/Test/Database/LSMTree/Normal/Examples.hs
+++ test/Test/Database/LSMTree/Normal/Examples.hs
@@ -2,31 +2,37 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Test.Database.LSMTree.Normal.Examples (tests) where
 
-import           Control.Tracer (nullTracer)
 import           Data.ByteString (ByteString)
+import           Control.Tracer (nullTracer)
 import qualified System.FS.API as FS
+
+import           Database.LSMTree.Normal as R
+
 import           Test.Tasty (TestTree, testGroup)
 import           Test.Tasty.HUnit
 import           Test.Util.FS (withTempIOHasBlockIO)
 
-import           Database.LSMTree.Normal
 
 tests :: TestTree
-tests = testGroup "Normal.Examples"
-    [ testCaseSteps "blobs" $ \ info ->
-        withTempIOHasBlockIO "test" $ \hfs hbio ->
-        withSession nullTracer hfs hbio (FS.mkFsPath []) $ \sess ->
-        withTable @_ @ByteString @ByteString @ByteString sess defaultTableConfig $ \tbh -> do
-            inserts [("key1", "value1", Just "blob1")] tbh
+tests =
+    testGroup "Normal.Examples"
+      [ testCaseSteps "unit_blobs"         unit_blobs
+      ]
 
-            res <- lookups ["key1"] tbh
-            info (show res)
+unit_blobs :: (String -> IO ()) -> Assertion
+unit_blobs info =
+    withTempIOHasBlockIO "test" $ \hfs hbio ->
+    withSession nullTracer hfs hbio (FS.mkFsPath []) $ \sess -> do
+      tbh <- new @_ @ByteString @ByteString @ByteString sess defaultTableConfig
+      inserts [("key1", "value1", Just "blob1")] tbh
 
-            case res of
-              [FoundWithBlob val bref] -> do
-                val @?= "value1"
-                blob <- retrieveBlobs sess [bref]
-                info (show blob)
-                blob @?= ["blob1"]
-              _ -> assertFailure "expected FoundWithBlob"
-    ]
+      res <- lookups ["key1"] tbh
+      info (show res)
+
+      case res of
+        [FoundWithBlob val bref] -> do
+          val @?= "value1"
+          blob <- retrieveBlobs sess [bref]
+          info (show blob)
+          blob @?= ["blob1"]
+        _ -> assertFailure "expected FoundWithBlob"
