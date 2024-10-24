7bf880d3b19c6cf21bbb7d53d480e449701085a7
Joris Dral <joris@well-typed.com>
2024-10-24 10:13:56 +0200

    Remove resolve-related functions from the `D.L.I.Entry` module.
    These functions are only used in tests. Moreover, the tests can replace these
    functions by 'Semigroup' functions like 'sconcat'.

--- src/Database/LSMTree/Internal/Entry.hs
+++ src/Database/LSMTree/Internal/Entry.hs
@@ -8,19 +8,11 @@ module Database.LSMTree.Internal.Entry (
     -- * Value resolution/merging
   , combine
   , combineMaybe
-  , combinesMonoidal
-  , combinesNormal
-  , resolveEntriesNormal
-  , resolveEntriesMonoidal
   ) where
 
 import           Control.DeepSeq (NFData (..))
 import           Data.Bifoldable (Bifoldable (..))
 import           Data.Bifunctor (Bifunctor (..))
-import           Data.List.NonEmpty (NonEmpty)
-import qualified Data.List.NonEmpty as NE
-import qualified Database.LSMTree.Internal.Monoidal as Monoidal
-import qualified Database.LSMTree.Internal.Normal as Normal
 
 data Entry v blobref
     = Insert !v
@@ -88,6 +80,10 @@ unNumEntries (NumEntries x) = x
   Value resolution/merging
 -------------------------------------------------------------------------------}
 
+-- | As long as values are a semigroup, an Entry is too
+instance Semigroup v => Semigroup (Entry v blob) where
+  e1 <> e2 = combine (<>) e1 e2
+
 -- | Given a value-merge function, combine entries
 combine :: (v -> v -> v) -> Entry v blobref -> Entry v blobref -> Entry v blobref
 combine _ e@Delete            _                       = e
@@ -102,24 +98,3 @@ combineMaybe :: (v -> v -> v) -> Maybe (Entry v blobref) -> Maybe (Entry v blobr
 combineMaybe _ e1 Nothing          = e1
 combineMaybe _ Nothing e2          = e2
 combineMaybe f (Just e1) (Just e2) = Just $! combine f e1 e2
-
-combinesMonoidal :: (v -> v -> v) -> NonEmpty (Entry v blob) -> Entry v blob
-combinesMonoidal f = foldr1 (combine f) -- short-circuit fold
-
-combinesNormal :: NonEmpty (Entry v blob) -> Entry v blob
-combinesNormal = NE.head
-
--- | Returns 'Nothing' if the combined entries can not be mapped to an
--- 'Normal.Update'.
-resolveEntriesNormal ::
-     NonEmpty (Entry v blob)
-  -> Maybe (Normal.Update v blob)
-resolveEntriesNormal _ = error "about to be removed"
-
--- | Returns 'Nothing' if the combined entries can not be mapped to an
--- 'Monoidal.Update'.
-resolveEntriesMonoidal ::
-     (v -> v -> v)
-  -> NonEmpty (Entry v blob)
-  -> Maybe (Monoidal.Update v)
-resolveEntriesMonoidal _ _ = error "about to be removed"
