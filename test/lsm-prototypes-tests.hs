be44d8dbc083be15073b4abd7a4eb5fba3afcca6
Matthias Heinzel <matthias@well-typed.com>
2024-11-12 21:57:02 +0100

    prototype: split test modules

--- test/lsm-prototypes-tests.hs
+++ test/lsm-prototypes-tests.hs
@@ -3,10 +3,14 @@ module Main (main) where
 import           Test.Tasty
 
 import qualified FormatPage
+import qualified ScheduledMergesTest
 import qualified ScheduledMergesTestQLS
 
 main :: IO ()
 main = defaultMain $ testGroup "prototype" [
-      ScheduledMergesTestQLS.tests,
+      testGroup "ScheduledMerges" [
+        ScheduledMergesTest.tests,
+        ScheduledMergesTestQLS.tests
+      ],
       FormatPage.tests
     ]
