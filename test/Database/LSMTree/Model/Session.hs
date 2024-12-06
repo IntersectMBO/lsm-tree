ea0c95887811241e1086434723f26b2c093f03c2
Joris Dral <joris@well-typed.com>
2024-12-06 15:12:55 +0100

    Implement n-way unions in the model

--- test/Database/LSMTree/Model/Session.hs
+++ test/Database/LSMTree/Model/Session.hs
@@ -73,6 +73,7 @@ module Database.LSMTree.Model.Session (
   , duplicate
     -- * Table union
   , union
+  , unions
   ) where
 
 import           Control.Monad (when)
@@ -204,6 +205,10 @@ data Err =
   | ErrSnapshotWrongType
   | ErrBlobRefInvalidated
   | ErrCursorClosed
+    -- | Passed zero tables to 'unions'
+  | ErrUnionsZeroTables
+    -- | Passed one table to 'unions'
+  | ErrUnionsOneTable
   deriving stock (Show, Eq)
 
 {-------------------------------------------------------------------------------
@@ -628,3 +633,22 @@ union r th1 th2 = do
   (_, t1) <- guardTableIsOpen th1
   (_, t2) <- guardTableIsOpen th2
   newTableWith TableConfig $ Model.union r t1 t2
+
+unions ::
+     ( MonadState Model m
+     , MonadError Err m
+     , C k v b
+     )
+  => ResolveSerialisedValue v
+  -> V.Vector (Table k v b)
+  -> m (Table k v b)
+unions r tables
+  | n == 0 = throwError ErrUnionsZeroTables
+  | n == 1 = throwError ErrUnionsOneTable
+  | otherwise = do
+      tables' <- V.forM tables $ \table -> do
+        (_, table') <- guardTableIsOpen table
+        pure table'
+      newTableWith TableConfig $ Model.unions r tables'
+  where
+    n = V.length tables
