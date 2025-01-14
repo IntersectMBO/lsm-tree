4e414232fd7d9e1be8e537f9f67d43af6fa7bed1
Joris Dral <joris@well-typed.com>
2025-01-14 15:34:28 +0100

    TOSQUASH: Test exception safety for `WriteBufferBlobs` functions

--- test/Test/Database/LSMTree/Internal/WriteBufferBlobs/FS.hs
+++ test/Test/Database/LSMTree/Internal/WriteBufferBlobs/FS.hs
@@ -4,6 +4,9 @@ import           Control.Concurrent.Class.MonadSTM.Strict
 import           Control.Monad
 import           Control.Monad.Class.MonadThrow
 import           Control.RefCount
+import           Database.LSMTree.Extras.Generators ()
+import           Database.LSMTree.Internal.BlobRef (readRawBlobRef)
+import           Database.LSMTree.Internal.Serialise (SerialisedBlob)
 import           Database.LSMTree.Internal.WriteBufferBlobs
 import           System.FS.API
 import           System.FS.Sim.Error hiding (genErrors)
@@ -14,30 +17,38 @@ import           Test.Util.FS
 
 tests :: TestTree
 tests = testGroup "Test.Database.LSMTree.Internal.WriteBufferBlobs.FS" [
-      testProperty "prop_fault_openRelease" prop_fault_openRelease
+      testProperty "prop_fault_WriteBufferBlobs" prop_fault_WriteBufferBlobs
     ]
 
 -- Test that opening and releasing a 'WriteBufferBlobs' properly cleans handles
--- and files in the presence of disk faults.
+-- and files in the presence of disk faults. Also test that we can write then
+-- read blobs correctly in the presence of disk faults.
 --
 -- By testing 'open', we also test 'new'.
-prop_fault_openRelease ::
+prop_fault_WriteBufferBlobs ::
      Bool -- ^ create the file or not
   -> AllowExisting
   -> NoCleanupErrors
+  -> Errors
   -> NoCleanupErrors
+  -> SerialisedBlob
+  -> SerialisedBlob
   -> Property
-prop_fault_openRelease doCreateFile ae
+prop_fault_WriteBufferBlobs doCreateFile ae
   (NoCleanupErrors openErrors)
-  (NoCleanupErrors releaseErrors) =
+  errs
+  (NoCleanupErrors releaseErrors)
+  b1 b2 =
     ioProperty $
     withSimErrorHasFS propPost MockFS.empty emptyErrors $ \hfs fsVar errsVar -> do
       when doCreateFile $
         withFile hfs path (WriteMode MustBeNew) $ \_ -> pure ()
       eith <- try @_ @FsError $
-        bracket (acquire hfs errsVar) (release errsVar) $ \_wbb -> do
+        bracket (acquire hfs errsVar) (release errsVar) $ \wbb -> do
           fs' <- atomically $ readTMVar fsVar
-          pure $ propNumOpenHandles 1 fs' .&&. propNumDirEntries root 1 fs'
+          let prop = propNumOpenHandles 1 fs' .&&. propNumDirEntries root 1 fs'
+          props <- blobRoundtrips hfs errsVar wbb
+          pure (prop .&&. props)
       pure $ case eith of
         Left{}     -> do
           label "FsError" $ property True
@@ -48,6 +59,16 @@ prop_fault_openRelease doCreateFile ae
     path = mkFsPath ["wbb"]
 
     acquire hfs errsVar = withErrors errsVar openErrors $ open hfs path ae
+
+    -- Test that we can roundtrip blobs
+    blobRoundtrips hfs errsVar wbb = withErrors errsVar errs $ do
+        props <-
+          forM [b1, b2] $ \b -> do
+            bspan <- addBlob hfs wbb b
+            let bref = mkRawBlobRef wbb bspan
+            b' <- readRawBlobRef hfs bref
+            pure (b === b')
+        pure $ conjoin props
 
     release errsVar wbb = withErrors errsVar releaseErrors $ releaseRef wbb
 
