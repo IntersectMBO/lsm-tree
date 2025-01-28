3d9909d06d77f9a6c279fc01d4cefd1c23252f38
Wen Kokke <wenkokke@users.noreply.github.com>
2025-01-28 13:52:01 +0000

    fix: specialise writeFileSnapshotMetaData/readFileSnapshotMetaData
    Co-authored-by: Joris Dral <joris@well-typed.com>

--- src/Database/LSMTree/Internal/Snapshot/Codec.hs
+++ src/Database/LSMTree/Internal/Snapshot/Codec.hs
@@ -76,6 +76,14 @@ isCompatible otherVersion = do
   Writing and reading files
 -------------------------------------------------------------------------------}
 
+{-# SPECIALIZE
+  writeFileSnapshotMetaData ::
+       HasFS IO h
+    -> FsPath
+    -> FsPath
+    -> SnapshotMetaData
+    -> IO ()
+  #-}
 -- | Encode 'SnapshotMetaData' and write it to 'SnapshotMetaDataFile'.
 --
 -- In the presence of exceptions, newly created files will not be removed. It is
@@ -96,6 +104,13 @@ writeFileSnapshotMetaData hfs contentPath checksumPath snapMetaData = do
         checksumFile = Map.singleton checksumFileName checksum
     writeChecksumsFile hfs checksumPath checksumFile
 
+{-# SPECIALIZE
+  readFileSnapshotMetaData ::
+       HasFS IO h
+    -> FsPath
+    -> FsPath
+    -> IO (Either DeserialiseFailure SnapshotMetaData)
+  #-}
 -- | Read from 'SnapshotMetaDataFile' and attempt to decode it to
 -- 'SnapshotMetaData'.
 readFileSnapshotMetaData ::
