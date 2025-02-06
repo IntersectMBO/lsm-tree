07fdff6248bccb8796936a6530ab67df2b05edc5
Wen Kokke <wenkokke@users.noreply.github.com>
2025-02-06 17:27:05 +0000

    refactor(issue558): extract flipRandomBitInRandomFile from prop_flipSnapshotBit

--- test/Test/Util/QC.hs
+++ test/Test/Util/QC.hs
@@ -2,12 +2,15 @@ module Test.Util.QC (
     testClassLaws
   , testClassLawsWith
   , Proxy (..)
+  , Choice
+  , getChoice
   ) where
 
 import           Data.Proxy (Proxy (..))
+import           Data.Word (Word64)
 import           Test.QuickCheck.Classes (Laws (..))
 import           Test.Tasty (TestTree, testGroup)
-import           Test.Tasty.QuickCheck (Property, testProperty)
+import           Test.Tasty.QuickCheck (Property, Arbitrary (..), testProperty)
 
 testClassLaws :: String -> Laws -> TestTree
 testClassLaws typename laws = testClassLawsWith typename laws testProperty
@@ -21,4 +24,17 @@ testClassLawsWith typename Laws {lawsTypeclass, lawsProperties} k =
     [ k name prop
     | (name, prop) <- lawsProperties ]
 
+
+-- | A 'Choice' of a uniform random number in a range where shrinking picks smaller numbers.
+newtype Choice = Choice Word64
+  deriving stock (Show, Eq)
+  deriving newtype (Arbitrary)
+
+getChoice :: (Integral a) => Choice -> (a, a) -> a
+getChoice (Choice n) (l, u) = fromIntegral (((ni * (ui - li)) `div` mi) + li)
+  where
+    ni = toInteger n
+    li = toInteger l
+    ui = toInteger u
+    mi = toInteger (maxBound :: Word64)
 
