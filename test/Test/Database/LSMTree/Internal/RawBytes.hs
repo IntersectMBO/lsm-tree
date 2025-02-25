7665b6d140702cac7b142f945ffb8a1b14edac60
Wolfgang Jeltsch <wolfgang@well-typed.com>
2025-02-25 23:01:41 +0200

    Correct the shape of `RawBytes` property names

--- test/Test/Database/LSMTree/Internal/RawBytes.hs
+++ test/Test/Database/LSMTree/Internal/RawBytes.hs
@@ -18,17 +18,17 @@ tests = testGroup "Test.Database.LSMTree.Internal.RawBytes" $
         [
             testGroup "Eq laws" $
             [
-                testProperty "Reflexivity"  propEqReflexivity,
-                testProperty "Symmetry"     propEqSymmetry,
-                testProperty "Transitivity" propEqTransitivity,
-                testProperty "Negation"     propEqNegation
+                testProperty "Reflexivity"  prop_eqReflexivity,
+                testProperty "Symmetry"     prop_eqSymmetry,
+                testProperty "Transitivity" prop_eqTransitivity,
+                testProperty "Negation"     prop_eqNegation
             ],
             testGroup "Ord laws" $
             [
-                testProperty "Comparability" propOrdComparability,
-                testProperty "Transitivity"  propOrdTransitivity,
-                testProperty "Reflexivity"   propOrdReflexivity,
-                testProperty "Antisymmetry"  propOrdAntisymmetry
+                testProperty "Comparability" prop_ordComparability,
+                testProperty "Transitivity"  prop_ordTransitivity,
+                testProperty "Reflexivity"   prop_ordReflexivity,
+                testProperty "Antisymmetry"  prop_ordAntisymmetry
             ]
         ]
 
@@ -42,16 +42,16 @@ withFirstBlockSizeInfo firstBlock
 
 -- ** 'Eq' laws
 
-propEqReflexivity :: RawBytes -> Property
-propEqReflexivity block = block === block
+prop_eqReflexivity :: RawBytes -> Property
+prop_eqReflexivity block = block === block
 
-propEqSymmetry :: RawBytes -> RawBytes -> Property
-propEqSymmetry block1 block2 = (block1 == block2) === (block2 == block1)
+prop_eqSymmetry :: RawBytes -> RawBytes -> Property
+prop_eqSymmetry block1 block2 = (block1 == block2) === (block2 == block1)
 
-propEqTransitivity :: Property
-propEqTransitivity = mapSize (const 3)     $
-                     withDiscardRatio 1000 $
-                     untunedProp
+prop_eqTransitivity :: Property
+prop_eqTransitivity = mapSize (const 3)     $
+                      withDiscardRatio 1000 $
+                      untunedProp
     where
 
     untunedProp :: RawBytes -> RawBytes -> RawBytes -> Property
@@ -59,25 +59,25 @@ propEqTransitivity = mapSize (const 3)     $
         = withFirstBlockSizeInfo block1 $
           block1 == block2 && block2 == block3 ==> block1 === block3
 
-propEqNegation :: RawBytes -> RawBytes -> Property
-propEqNegation block1 block2 = (block1 /= block2) === not (block1 == block2)
+prop_eqNegation :: RawBytes -> RawBytes -> Property
+prop_eqNegation block1 block2 = (block1 /= block2) === not (block1 == block2)
 
 -- ** 'Ord' laws
 
-propOrdComparability :: RawBytes -> RawBytes -> Property
-propOrdComparability block1 block2 = block1 <= block2 .||. block2 <= block1
+prop_ordComparability :: RawBytes -> RawBytes -> Property
+prop_ordComparability block1 block2 = block1 <= block2 .||. block2 <= block1
 
-propOrdTransitivity :: RawBytes -> RawBytes -> RawBytes -> Property
-propOrdTransitivity block1 block2 block3
+prop_ordTransitivity :: RawBytes -> RawBytes -> RawBytes -> Property
+prop_ordTransitivity block1 block2 block3
     = block1 <= block2 && block2 <= block3 ==> block1 <= block3
 
-propOrdReflexivity :: RawBytes -> Bool
-propOrdReflexivity block = block <= block
+prop_ordReflexivity :: RawBytes -> Bool
+prop_ordReflexivity block = block <= block
 
-propOrdAntisymmetry :: Property
-propOrdAntisymmetry = mapSize (const 4)    $
-                      withDiscardRatio 100 $
-                      untunedProp
+prop_ordAntisymmetry :: Property
+prop_ordAntisymmetry = mapSize (const 4)    $
+                       withDiscardRatio 100 $
+                       untunedProp
     where
 
     untunedProp :: RawBytes -> RawBytes -> Property
