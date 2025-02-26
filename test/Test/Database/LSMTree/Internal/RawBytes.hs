be22c3f202fe0bca61ea94dd0d6949fb99ced9aa
Wolfgang Jeltsch <wolfgang@well-typed.com>
2025-02-26 20:16:50 +0200

    Improve `RawBytes` properties regarding equal blocks

--- test/Test/Database/LSMTree/Internal/RawBytes.hs
+++ test/Test/Database/LSMTree/Internal/RawBytes.hs
@@ -6,8 +6,8 @@ module Test.Database.LSMTree.Internal.RawBytes (tests) where
 import           Database.LSMTree.Extras.Generators ()
 import           Database.LSMTree.Internal.RawBytes (RawBytes)
 import qualified Database.LSMTree.Internal.RawBytes as RawBytes (size)
-import           Test.QuickCheck (Property, collect, mapSize, withDiscardRatio,
-                     (.||.), (===), (==>))
+import           Test.QuickCheck (Property, classify, collect, mapSize,
+                     withDiscardRatio, withMaxSuccess, (.||.), (===), (==>))
 import           Test.Tasty (TestTree, testGroup)
 import           Test.Tasty.QuickCheck (testProperty)
 
@@ -34,6 +34,11 @@ tests = testGroup "Test.Database.LSMTree.Internal.RawBytes" $
 
 -- * Utilities
 
+twoBlocksProp :: String -> RawBytes -> RawBytes -> Property -> Property
+twoBlocksProp msgAddition block1 block2
+    = withMaxSuccess 10000 .
+      classify (block1 == block2) ("equal blocks" ++ msgAddition)
+
 withFirstBlockSizeInfo :: RawBytes -> Property -> Property
 withFirstBlockSizeInfo firstBlock
     = collect ("Size of first block is " ++ show (RawBytes.size firstBlock))
@@ -46,7 +51,8 @@ prop_eqReflexivity :: RawBytes -> Property
 prop_eqReflexivity block = block === block
 
 prop_eqSymmetry :: RawBytes -> RawBytes -> Property
-prop_eqSymmetry block1 block2 = (block1 == block2) === (block2 == block1)
+prop_eqSymmetry block1 block2 = twoBlocksProp "" block1 block2 $
+                                (block1 == block2) === (block2 == block1)
 
 prop_eqTransitivity :: Property
 prop_eqTransitivity = mapSize (const 3)     $
@@ -60,16 +66,21 @@ prop_eqTransitivity = mapSize (const 3)     $
           block1 == block2 && block2 == block3 ==> block1 === block3
 
 prop_eqNegation :: RawBytes -> RawBytes -> Property
-prop_eqNegation block1 block2 = (block1 /= block2) === not (block1 == block2)
+prop_eqNegation block1 block2 = twoBlocksProp "" block1 block2 $
+                                (block1 /= block2) === not (block1 == block2)
 
 -- ** 'Ord' laws
 
 prop_ordComparability :: RawBytes -> RawBytes -> Property
-prop_ordComparability block1 block2 = block1 <= block2 .||. block2 <= block1
+prop_ordComparability block1 block2 = twoBlocksProp "" block1 block2 $
+                                      block1 <= block2 .||. block2 <= block1
 
 prop_ordTransitivity :: RawBytes -> RawBytes -> RawBytes -> Property
 prop_ordTransitivity block1 block2 block3
-    = block1 <= block2 && block2 <= block3 ==> block1 <= block3
+    = twoBlocksProp " front-side"   block1 block2 $
+      twoBlocksProp " rear-side"    block2 block3 $
+      twoBlocksProp " at the edges" block1 block3 $
+      block1 <= block2 && block2 <= block3 ==> block1 <= block3
 
 prop_ordReflexivity :: RawBytes -> Bool
 prop_ordReflexivity block = block <= block
