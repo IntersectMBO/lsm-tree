800bc1f0b14a43478b35528ca34dfd24568b0e3a
Duncan Coutts <duncan@well-typed.com>
2025-03-24 10:51:07 +0000

    bloomfilter: Add a simple construction benchmark
    For testing the bloomfilter lib in isolation, rather than the use in the
    lsm-tree lib.

---
+++ bloomfilter/bench/bloomfilter-bench.hs
@@ -0,0 +1,39 @@
+module Main where
+
+import qualified Data.BloomFilter as B
+import qualified Data.BloomFilter.Easy as B
+import           Data.BloomFilter.Hash (Hashable (..), hash64)
+
+import           Data.Word (Word64)
+import           System.Random
+
+import           Criterion.Main
+
+main :: IO ()
+main =
+    defaultMain [
+      env newStdGen $ \g0 ->
+      bench "construct bloom m=1e6 fpr=1%" $
+        whnf (constructBloom 1_000_000 0.01) g0
+
+    , env newStdGen $ \g0 ->
+      bench "construct bloom m=1e6 fpr=0.1%" $
+        whnf (constructBloom 1_000_000 0.001) g0
+
+    , env newStdGen $ \g0 ->
+      bench "construct bloom m=1e7 fpr=0.1%" $
+        whnf (constructBloom 10_000_000 0.001) g0
+    ]
+
+constructBloom :: Int -> Double -> StdGen -> B.Bloom Word64
+constructBloom n fpr g0 =
+    let (bits, hashes) = B.suggestSizing n fpr in
+    B.unfold hashes bits nextElement (g0, 0)
+  where
+    nextElement :: (StdGen, Int) -> Maybe (Word64, (StdGen, Int))
+    nextElement (!g, !i)
+      | i >= n    = Nothing
+      | otherwise = Just (x, (g', i+1))
+        where
+          (!x, !g') = uniform g
+
