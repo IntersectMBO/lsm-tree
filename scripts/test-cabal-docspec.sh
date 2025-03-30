798fb2e42ed181495ad11927a78cd95763c67ce1
Recursion Ninja <github@recursion.ninja>
2025-03-30 12:23:50 -0400

    Adding benchmark for table unions

--- scripts/test-cabal-docspec.sh
+++ scripts/test-cabal-docspec.sh
@@ -44,7 +44,6 @@ ${cabal_docspec} \
     --extra-package blockio \
     --extra-package blockio:sim \
     --extra-package directory \
-    --extra-package primitive \
     --extra-package lsm-tree:prototypes \
     --extra-package process \
     || exit 1
