45b8efc81cb857977ce20606b66e8347a45178ec
Wen Kokke <wenkokke@users.noreply.github.com>
2025-03-31 15:11:48 +0100

    fix: do not fail fast in pre-commit.sh

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -5,23 +5,40 @@
 # > ln scripts/pre-commit.sh .git/hooks/pre-commit.sh
 #
 
+# POSIX compliant method for 'pipefail':
+fail=$(mktemp)
+
 # Check for unstaged changes in Haskell files
-unstaged_haskell_files="$(git ls-files --exclude-standard --no-deleted --deduplicate --modified '*.hs')"
+unstaged_haskell_files="$(git ls-files --exclude-standard --no-deleted --deduplicate --modified '*.hs' || echo > "$fail")"
 if [ ! "${unstaged_haskell_files}" = "" ]; then
     echo "Found unstaged Haskell files"
     echo "${unstaged_haskell_files}"
-    exit 1
 fi
 
 # Check for unstaged changes in Cabal files
-unstaged_cabal_files="$(git ls-files --exclude-standard --no-deleted --deduplicate --modified '*.cabal')"
+unstaged_cabal_files="$(git ls-files --exclude-standard --no-deleted --deduplicate --modified '*.cabal' || echo > "$fail")"
 if [ ! "${unstaged_cabal_files}" = "" ]; then
     echo "Found unstaged Cabal files"
     echo "${unstaged_cabal_files}"
-    exit 1
 fi
 
-# Run various checks and formatters
-./scripts/check-cabal.sh || exit 1
-./scripts/format-cabal.sh || exit 1
-./scripts/format-stylish.sh || exit 1
+# Check Cabal files with cabal
+./scripts/check-cabal.sh || echo > "$fail"
+echo
+
+# Format Cabal files with cabal-fmt
+./scripts/format-cabal.sh || echo > "$fail"
+echo
+
+# Format Haskell files with stylish-haskell
+./scripts/format-stylish.sh || echo > "$fail"
+echo
+
+# Check whether or not any subcommand failed:
+if [ -s "$fail" ]; then
+    rm "$fail"
+    exit 1
+else
+    rm "$fail"
+    exit 0
+fi
