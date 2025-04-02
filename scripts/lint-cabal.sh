ad986a48b88e607b6f5be21d8ba55ff882633b34
Wen Kokke <wenkokke@users.noreply.github.com>
2025-04-02 10:00:22 +0100

    fix: refactor pre-commit scripts to better match CI

--- scripts/lint-cabal.sh
+++ scripts/lint-cabal.sh
@@ -7,8 +7,8 @@ if [ "${cabal}" = "" ]; then
     exit 1
 fi
 
-# Check Cabal files with cabal
-echo "Checking Cabal source files with cabal"
+# Lint Cabal files with cabal
+echo "Linting Cabal source files with cabal"
 # shellcheck disable=SC2016
 if ! git ls-files --exclude-standard --no-deleted --deduplicate '*.cabal' | xargs -L 1 sh -c 'echo "$0" && cd "$(dirname "$0")" && cabal check'; then
     exit 1
