e72333edacb774973df9722466b51bf06a7373bb
Duncan Coutts <duncan@well-typed.com>
2025-04-14 19:16:53 +0100

    bloomfilter: establish a common API for hash-based insert and elem
    In preparatioon for adding a new blocked bloom filter implementation,
    make various adjustments to the existing API so that it will be easier
    for the new implementation to match it.
    
    We provide a low level API for bloom filter inserts and elem operations.
    These allow for sharing a single hash calculation across many filters,
    and potentially allows for prefetching.
    
    Now instead of a CheapHashes type and operations in the Hash module,
    each of the two implementations (Classic and Blocked) provide a Hashes
    type and constructor. The classic one uses the (renamed) CheapHashes,
    while the Blocked one uses its own scheme.

--- bloomfilter/src/Data/BloomFilter/Hash.hs
+++ bloomfilter/src/Data/BloomFilter/Hash.hs
@@ -15,22 +15,15 @@ module Data.BloomFilter.Hash (
     Incremental (..),
     HashState,
     incrementalHash,
-    -- * Compute a family of hash values
-    CheapHashes (..),
-    evalHashes,
-    makeHashes,
 ) where
 
 import           Control.Monad (forM_)
 import           Control.Monad.ST (ST, runST)
-import           Data.Bits (unsafeShiftR)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
 import           Data.Char (ord)
 import qualified Data.Primitive.ByteArray as P
-import           Data.Primitive.Types (Prim (..))
 import           Data.Word (Word32, Word64)
-import           GHC.Exts (Int#, uncheckedIShiftL#, (+#))
 import qualified XXH3
 
 -- | A hash value is 64 bits wide.
@@ -138,133 +131,3 @@ incrementalHash seed f = runST $ do
     XXH3.xxh3_64bit_reset_withSeed s seed
     f (HashState s)
     XXH3.xxh3_64bit_digest s
-
--------------------------------------------------------------------------------
--- CheapHashes
--------------------------------------------------------------------------------
-
--- | A pair of hashes used for a double hashing scheme.
---
--- See 'evalCheapHashes'.
-data CheapHashes a = CheapHashes !Hash !Hash
-  deriving Show
-type role CheapHashes nominal
-
-instance Prim (CheapHashes a) where
-    sizeOfType# _ = 16#
-    alignmentOfType# _ = 8#
-
-    indexByteArray# ba i = CheapHashes
-        (indexByteArray# ba (indexLo i))
-        (indexByteArray# ba (indexHi i))
-    readByteArray# ba i s1 =
-        case readByteArray# ba (indexLo i) s1 of { (# s2, lo #) ->
-        case readByteArray# ba (indexHi i) s2 of { (# s3, hi #) ->
-        (# s3, CheapHashes lo hi #)
-        }}
-    writeByteArray# ba i (CheapHashes lo hi) s =
-        writeByteArray# ba (indexHi i) hi (writeByteArray# ba (indexLo i) lo s)
-
-    indexOffAddr# ba i = CheapHashes
-        (indexOffAddr# ba (indexLo i))
-        (indexOffAddr# ba (indexHi i))
-    readOffAddr# ba i s1 =
-        case readOffAddr# ba (indexLo i) s1 of { (# s2, lo #) ->
-        case readOffAddr# ba (indexHi i) s2 of { (# s3, hi #) ->
-        (# s3, CheapHashes lo hi #)
-        }}
-    writeOffAddr# ba i (CheapHashes lo hi) s =
-        writeOffAddr# ba (indexHi i) hi (writeOffAddr# ba (indexLo i) lo s)
-
-indexLo :: Int# -> Int#
-indexLo i = uncheckedIShiftL# i 1#
-
-indexHi :: Int# -> Int#
-indexHi i = uncheckedIShiftL# i 1# +# 1#
-
-{- Note [Original CheapHashes]
-
-Compute a list of 32-bit hashes relatively cheaply.  The value to
-hash is inspected at most twice, regardless of the number of hashes
-requested.
-
-We use a variant of Kirsch and Mitzenmacher's technique from \"Less
-Hashing, Same Performance: Building a Better Bloom Filter\",
-<http://www.eecs.harvard.edu/~kirsch/pubs/bbbf/esa06.pdf>.
-
-Where Kirsch and Mitzenmacher multiply the second hash by a
-coefficient, we shift right by the coefficient.  This offers better
-performance (as a shift is much cheaper than a multiply), and the
-low order bits of the final hash stay well mixed.
-
--}
-
-{- Note: [CheapHashes]
-
-On the first glance the 'evalCheapHashes' scheme seems dubious.
-
-Firstly, it's original performance motivation is dubious.
-
-> multiply the second hash by a coefficient
-
-While the scheme double hashing scheme is presented in
-theoretical analysis as
-
-    g(i) = a + i * b
-
-In practice it's implemented in a loop which looks like
-
-    g[0] = a
-    for (i = 1; i < k; i++) {
-        a += b;
-        g[i] = a;
-    }
-
-I.e. with just an addition.
-
-Secondly there is no analysis anywhere about the
-'evalCheapHashes' scheme.
-
-Peter Dillinger's thesis (Adaptive Approximate State Storage)
-discusses various fast hashing schemes (section 6.5),
-mentioning why ordinary "double hashing" is weak scheme.
-
-Issue 1: when second hash value is bad, e.g. not coprime with bloom filters size in bits,
-we can get repetitions (worst case 0, or m/2).
-
-Issue 2: in bloom filter scenario, whether we do a + i * b or h0 - i * b' (with b' = -b)
-as we probe all indices (as set) doesn't matter, not sequentially (like in hash table).
-So we lose one bit entropy.
-
-Issue 3: the scheme is prone to partial overlap.
-Two values with the same second hash value could overlap on many indices.
-
-Then Dillinger discusses various schemes which solve this issue.
-
-The CheapHashes scheme seems to avoid these cuprits.
-This is probably because it uses most of the bits of the second hash, even in m = 2^n scenarios.
-(normal double hashing and enhances double hashing don't use the high bits or original hash then).
-TL;DR CheapHashes seems to work well in practice.
-
-For the record: RocksDB uses an own scheme as well,
-where first hash is used to pick a cache line, and second one to generate probes inside it.
-https://github.com/facebook/rocksdb/blob/096fb9b67d19a9a180e7c906b4a0cdb2b2d0c1f6/util/bloom_impl.h
-
--}
-
--- | Evaluate 'CheapHashes' family.
---
--- \[
--- g_i = h_0 + \left\lfloor h_1 / 2^i \right\rfloor
--- \]
---
-evalHashes :: CheapHashes a -> Int -> Hash
-evalHashes (CheapHashes h1 h2) i = h1 + (h2 `unsafeShiftR` i)
-
--- | Create 'CheapHashes' structure.
---
--- It's simply hashes the value twice using seed 0 and 1.
-makeHashes :: Hashable a => a -> CheapHashes a
-makeHashes v = CheapHashes (hashSalt64 0 v) (hashSalt64 1 v)
-{-# SPECIALISE makeHashes :: BS.ByteString -> CheapHashes BS.ByteString #-}
-{-# INLINEABLE makeHashes #-}
--- src/Database/LSMTree/Internal/BloomFilterQuery1.hs
+++ src/Database/LSMTree/Internal/BloomFilterQuery1.hs
@@ -22,7 +22,6 @@ import           Control.Monad.ST (ST)
 
 import           Data.BloomFilter (Bloom)
 import qualified Data.BloomFilter as Bloom
-import qualified Data.BloomFilter.Hash as Bloom
 
 import           Database.LSMTree.Internal.Serialise (SerialisedKey)
 
@@ -94,8 +93,8 @@ bloomQueries !blooms !ks
     !rsN = V.length blooms
     !ksN = V.length ks
 
-    hs :: VP.Vector (Bloom.CheapHashes SerialisedKey)
-    !hs  = VP.generate ksN $ \i -> Bloom.makeHashes (V.unsafeIndex ks i)
+    hs :: VP.Vector (Bloom.Hashes SerialisedKey)
+    !hs  = VP.generate ksN $ \i -> Bloom.hashes (V.unsafeIndex ks i)
 
     -- Loop over all run indexes
     loop1 ::
@@ -120,7 +119,7 @@ bloomQueries !blooms !ks
         loop2 !res2 !resix2 !kix !b
           | kix == ksN = pure (res2, resix2)
           | let !h = hs `VP.unsafeIndex` kix
-          , Bloom.elemHashes h b = do
+          , Bloom.elemHashes b h = do
               -- Double the vector if we've reached the end.
               -- Note unsafeGrow takes the number to grow by, not the new size.
               res2' <- if resix2 == VPM.length res2
