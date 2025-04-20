a4483a98fdbae68296312a037bea56f90ba7eb81
Wolfgang Jeltsch <wolfgang@well-typed.com>
2025-04-20 15:19:35 +0300

    Add `IO` specialisation linting script

---
+++ scripts/lint-io-specialisations.sh
@@ -0,0 +1,44 @@
+#! /usr/bin/env sh
+
+set -e
+
+IFS='
+'
+
+absence_allowed_file=scripts/io-specialisations/absence-allowed
+absence_finder=scripts/io-specialisations/find-absent.sh
+
+export LC_COLLATE=C LC_TYPE=C
+
+printf 'Linting the main library regarding `IO` specialisations\n'
+
+if ! [ -f "$absence_allowed_file" ]
+then
+    printf 'There is no regular file `%s`.\n' "$absence_allowed_file"
+    exit 2
+fi >&2
+if ! sort -C "$absence_allowed_file"
+then
+    printf 'The entries in `%s` are not sorted.\n' "$absence_allowed_file"
+    exit 2
+fi >&2
+
+hs_files=$(
+    git ls-files \
+        --exclude-standard --no-deleted --deduplicate \
+        'src/**/*.hs'
+)
+absent=$(
+    "$absence_finder" $hs_files
+)
+missing=$(
+    printf '%s\n' "$absent" | sort | comm -23 - "$absence_allowed_file"
+)
+if [ -n "$missing" ]
+then
+    printf '`IO` specialisations for the following operations are '
+    printf 'missing:\n'
+    printf '%s\n' "$missing" | sed -e 's/.*/  * `&`/'
+    exit 1
+fi
+printf 'All required `IO` specialisations are present.\n'
