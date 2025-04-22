16b97287f57e375862b44ed1dff4c194e5e107a9
Duncan Coutts <duncan@well-typed.com>
2025-04-22 18:08:15 +0100

    bloomfilter: extend benchmark to blocked implementation

--- bloomfilter/bench/bloomfilter-bench.hs
+++ bloomfilter/bench/bloomfilter-bench.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import qualified Data.BloomFilter as B
+import qualified Data.BloomFilter.Blocked as B.Blocked
+import qualified Data.BloomFilter.Classic as B.Classic
 import           Data.BloomFilter.Hash (Hashable (..), hash64)
 
 import           Data.Word (Word64)
@@ -11,27 +12,47 @@ import           Criterion.Main
 main :: IO ()
 main =
     defaultMain [
-      env newStdGen $ \g0 ->
-      bench "construct bloom m=1e6 fpr=1%" $
-        whnf (constructBloom 1_000_000 0.01) g0
+      bgroup "Data.BloomFilter.Classic" [
+        env newStdGen $ \g0 ->
+        bench "construct m=1e6 fpr=1%" $
+          whnf (constructBloom_classic 1_000_000 0.01) g0
 
-    , env newStdGen $ \g0 ->
-      bench "construct bloom m=1e6 fpr=0.1%" $
-        whnf (constructBloom 1_000_000 0.001) g0
+      , env newStdGen $ \g0 ->
+        bench "construct m=1e6 fpr=0.1%" $
+          whnf (constructBloom_classic 1_000_000 0.001) g0
 
-    , env newStdGen $ \g0 ->
-      bench "construct bloom m=1e7 fpr=0.1%" $
-        whnf (constructBloom 10_000_000 0.001) g0
+      , env newStdGen $ \g0 ->
+        bench "construct m=1e7 fpr=0.1%" $
+          whnf (constructBloom_classic 10_000_000 0.001) g0
+      ]
+    , bgroup "Data.BloomFilter.Blocked" [
+        env newStdGen $ \g0 ->
+        bench "construct m=1e6 fpr=1%" $
+          whnf (constructBloom_blocked 1_000_000 0.01) g0
+
+      , env newStdGen $ \g0 ->
+        bench "construct m=1e6 fpr=0.1%" $
+          whnf (constructBloom_blocked 1_000_000 0.001) g0
+
+      , env newStdGen $ \g0 ->
+        bench "construct m=1e7 fpr=0.1%" $
+          whnf (constructBloom_blocked 10_000_000 0.001) g0
+      ]
     ]
 
-constructBloom :: Int -> Double -> StdGen -> B.Bloom Word64
-constructBloom n fpr g0 =
-    B.unfold (B.sizeForFPR fpr n) nextElement (g0, 0)
-  where
-    nextElement :: (StdGen, Int) -> Maybe (Word64, (StdGen, Int))
-    nextElement (!g, !i)
-      | i >= n    = Nothing
-      | otherwise = Just (x, (g', i+1))
-        where
-          (!x, !g') = uniform g
+constructBloom_classic :: Int -> Double -> StdGen -> B.Classic.Bloom Word64
+constructBloom_classic n fpr g0 =
+    B.Classic.unfold (B.Classic.sizeForFPR fpr n) (nextElement n) (g0, 0)
+
+constructBloom_blocked :: Int -> Double -> StdGen -> B.Blocked.Bloom Word64
+constructBloom_blocked n fpr g0 =
+    B.Blocked.unfold (B.Blocked.sizeForFPR fpr n) (nextElement n) (g0, 0)
+
+{-# INLINE nextElement #-}
+nextElement :: Int -> (StdGen, Int) -> Maybe (Word64, (StdGen, Int))
+nextElement !n (!g, !i)
+  | i >= n    = Nothing
+  | otherwise = Just (x, (g', i+1))
+    where
+      (!x, !g') = uniform g
 
