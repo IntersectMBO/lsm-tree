e9f008f456bfed09f36528d29cdd451c2bdea846
Joris Dral <joris@well-typed.com>
2025-05-08 16:03:15 +0200

    Move `FormatPage` tests into a dedicated test module

--- test-prototypes/Main.hs
+++ test-prototypes/Main.hs
@@ -2,13 +2,13 @@ module Main (main) where
 
 import           Test.Tasty
 
-import qualified FormatPage
+import qualified Test.FormatPage
 import qualified Test.ScheduledMerges
 import qualified Test.ScheduledMergesQLS
 
 main :: IO ()
 main = defaultMain $ testGroup "prototypes" [
-      FormatPage.tests
+      Test.FormatPage.tests
     , Test.ScheduledMerges.tests
     , Test.ScheduledMergesQLS.tests
     ]
