ea147aa8ca8752287c27f9220eef347302ef0fcc
Joris Dral <joris@well-typed.com>
2025-08-06 14:28:58 +0200

    Make `lsm-tree:demo` a test-suite that can run non-interactively

--- lsm-tree/app/Main.hs
+++ lsm-tree/app/Main.hs
@@ -1,9 +1,15 @@
 module Main (main) where
 
 import           Database.LSMTree.Demo (demo)
+import           System.Environment (getArgs)
 import           System.IO (BufferMode (..), hSetBuffering, stdout)
 
 main :: IO ()
 main = do
+  args <- getArgs
+  let isInteractive = args == ["Interactive"]
+  if isInteractive
+    then putStrLn "Running in Interactive mode"
+    else putStrLn "Running in NonInteractive mode"
   hSetBuffering stdout NoBuffering
-  demo
+  demo isInteractive
