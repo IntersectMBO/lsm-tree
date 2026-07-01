5e6090e504dbb1c1cfef22cb8e0991d59a49f76f
Joris Dral <joris@well-typed.com>
2026-07-01 16:23:45 +0200

    WIP

--- lsm-tree/src-core/Database/LSMTree/Internal/Page.hs
+++ lsm-tree/src-core/Database/LSMTree/Internal/Page.hs
@@ -11,9 +11,24 @@ module Database.LSMTree.Internal.Page (
   , singlePage
   , multiPage
   , pageSpanSize
+    -- * Constants
+  , pageSize
+    -- * I\/O
+  , readDiskPage
+  , skipPages
   ) where
 
 import           Control.DeepSeq (NFData (..))
+import           Control.Exception (assert)
+import           Control.Monad (guard)
+import           Control.Monad.Class.MonadThrow
+import           Control.Monad.Primitive (PrimMonad (..))
+import           Data.Primitive.ByteArray
+import           Data.Word (Word32)
+import           Database.LSMTree.Internal.BitMath (roundUpToPageSize)
+import           Database.LSMTree.Internal.RawPage (RawPage, unsafeMakeRawPage)
+import qualified System.FS.API as FS
+import           System.FS.API (Handle, HasFS)
 
 -- | A 0-based number identifying a disk page.
 newtype PageNo = PageNo { unPageNo :: Int }
@@ -65,3 +80,48 @@ multiPage i j = PageSpan i j
 pageSpanSize :: PageSpan -> NumPages
 pageSpanSize pspan = NumPages . toEnum $
     unPageNo (pageSpanEnd pspan) - unPageNo (pageSpanStart pspan) + 1
+
+{-------------------------------------------------------------------------------
+  Constants
+-------------------------------------------------------------------------------}
+
+pageSize :: Int
+pageSize = 4096
+
+{-------------------------------------------------------------------------------
+  I\/O
+-------------------------------------------------------------------------------}
+
+{-# SPECIALISE readDiskPage ::
+     HasFS IO h
+  -> Handle h
+  -> IO (Maybe RawPage) #-}
+-- | Returns 'Nothing' on EOF.
+readDiskPage ::
+     (MonadCatch m, PrimMonad m)
+  => HasFS m h
+  -> Handle h
+  -> m (Maybe RawPage)
+readDiskPage fs h = do
+    mba <- newPinnedByteArray pageSize
+    -- TODO: make sure no other exception type can be thrown
+    --
+    -- TODO: if FS.FsReachEOF is thrown as an injected disk fault, then we
+    -- incorrectly deduce that the file has no more contents. We should probably
+    -- use an explicit file pointer instead in the style of 'FilePointer'.
+    handleJust (guard . FS.isFsErrorType FS.FsReachedEOF) (\_ -> pure Nothing) $ do
+      bytesRead <- FS.hGetBufExactly fs h mba 0 (fromIntegral pageSize)
+      assert (fromIntegral bytesRead == pageSize) $ pure ()
+      ba <- unsafeFreezeByteArray mba
+      let !rawPage = unsafeMakeRawPage ba 0
+      pure (Just rawPage)
+
+-- | Move the handle's file pointer the given number of pages
+skipPages ::
+      HasFS m h
+   -> Handle h
+   -> Word32
+   -> m ()
+skipPages fs h len = do
+    let lenPages = fromIntegral (roundUpToPageSize len)
+    FS.hSeek fs h FS.RelativeSeek lenPages
--- lsm-tree/src-core/Database/LSMTree/Internal/RunReader/Keys.hs
+++ lsm-tree/src-core/Database/LSMTree/Internal/RunReader/Keys.hs
@@ -6,7 +6,6 @@
 module Database.LSMTree.Internal.RunReader.Keys (
     Keys (..)
   , unsafeNew
-  , OffsetKey (..)
   , RunInfo (..)
   , close
   , next
@@ -14,28 +13,25 @@ module Database.LSMTree.Internal.RunReader.Keys (
   ) where
 
 import           Control.Exception (assert)
-import           Control.Monad (guard, when)
+import           Control.Monad (when)
 import           Control.Monad.Class.MonadST (MonadST (..))
 import           Control.Monad.Class.MonadSTM (MonadSTM (..))
 import           Control.Monad.Class.MonadThrow (MonadCatch (..),
                      MonadMask (..))
 import           Control.Monad.Primitive (PrimMonad (..))
 import           Data.Maybe (isNothing)
-import           Data.Primitive.ByteArray (newPinnedByteArray,
-                     unsafeFreezeByteArray)
 import           Data.Primitive.MutVar (MutVar, newMutVar, readMutVar,
                      writeMutVar)
-import           Data.Primitive.PrimVar
-import           Data.Word (Word16, Word32)
-import           Database.LSMTree.Internal.BitMath (mulPageSize,
-                     roundUpToPageSize)
-import           Database.LSMTree.Internal.Index (Index)
-import qualified Database.LSMTree.Internal.Index as Index (search)
-import           Database.LSMTree.Internal.Page (NumPages, PageNo (..),
-                     PageSpan (..), getNumPages, nextPageNo)
-import           Database.LSMTree.Internal.RawPage
+import           Data.Primitive.PrimVar (PrimVar, modifyPrimVar, newPrimVar,
+                     readPrimVar, writePrimVar)
+import           Data.Word (Word16)
+import           Database.LSMTree.Internal.Page (NumPages, getNumPages,
+                     pageSize, readDiskPage, skipPages)
+import           Database.LSMTree.Internal.RawPage (RawPage,
+                     RawPageIndex (IndexEntry, IndexEntryOverflow, IndexNotPresent),
+                     rawPageIndex)
 import           Database.LSMTree.Internal.RunBuilder (RunDataCaching (..))
-import           Database.LSMTree.Internal.Serialise
+import           Database.LSMTree.Internal.Serialise (SerialisedKey)
 import qualified System.FS.API as FS
 import           System.FS.API (FsPath, Handle, HasFS)
 import qualified System.FS.BlockIO.API as FS
@@ -50,14 +46,10 @@ data Keys m h = Keys {
     , hasBlockIO     :: !(HasBlockIO m h)
     }
 
-data OffsetKey = NoOffsetKey | OffsetKey !SerialisedKey
-  deriving stock Show
-
 data RunInfo m h = RunInfo {
     hasFS       :: HasFS m h
   , hasBlockIO  :: HasBlockIO m h
   , kOpsPath    :: FsPath
-  , index       :: Index
   , dataCaching :: RunDataCaching
   , numPages    :: NumPages
   }
@@ -80,22 +72,21 @@ openKOpsFile runInfo = bracketOnError acq rel $ \h -> do
     rel = FS.hClose runInfo.hasFS
 
 {-# SPECIALISE unsafeNew ::
-     OffsetKey
-  -> RunInfo IO h
+     RunInfo IO h
   -> IO (Keys IO h) #-}
 unsafeNew :: forall m h.
      (MonadMask m, MonadSTM m, PrimMonad m)
-  => OffsetKey
-  -> RunInfo m h
+  => RunInfo m h
   -> m (Keys m h)
-unsafeNew offsetKey runInfo = do
+unsafeNew runInfo = do
     bracketOnError (openKOpsFile runInfo) (FS.hClose runInfo.hasFS) $ \kOpsHandle -> do
 
       FS.hAdviseAll runInfo.hasBlockIO kOpsHandle FS.AdviceSequential
 
-      (page, entryNo) <- seekFirstEntry kOpsHandle
+      -- Load first page from disk, if it exists.
+      page <- readDiskPage runInfo.hasFS kOpsHandle
 
-      currentEntryNo <- newPrimVar entryNo
+      currentEntryNo <- newPrimVar 0
       currentPage <- newMutVar page
       let reader = Keys {
               currentPage = currentPage
@@ -109,39 +100,6 @@ unsafeNew offsetKey runInfo = do
       when (isNothing page) $
         close reader
       pure reader
-  where
-    seekFirstEntry readerKOpsHandle =
-        case offsetKey of
-          NoOffsetKey -> do
-            -- Load first page from disk, if it exists.
-            firstPage <- readDiskPage runInfo.hasFS readerKOpsHandle
-            pure (firstPage, 0)
-          OffsetKey offset -> do
-            -- Use the index to find the page number for the key (if it exists).
-            let PageSpan pageNo pageEnd = Index.search offset runInfo.index
-            seekToDiskPage runInfo.hasFS pageNo readerKOpsHandle
-            readDiskPage runInfo.hasFS readerKOpsHandle >>= \case
-              Nothing ->
-                pure (Nothing, 0)
-              Just foundPage -> do
-                case rawPageFindKey foundPage offset of
-                  Just n ->
-                    -- Found an appropriate index within the index's page.
-                    pure (Just foundPage, n)
-
-                  _ -> do
-                    -- The index said that the key, if it were to exist, would
-                    -- live on pageNo, but then rawPageFindKey tells us that in
-                    -- fact there is no key greater than or equal to the given
-                    -- offset on this page.
-                    -- This tells us that the key does not exist, but that if it
-                    -- were to exist, it would be between the last key in this
-                    -- page and the first key in the next page.
-                    -- Thus the reader should be initialised to return keys
-                    -- starting from the next (non-overflow) page.
-                    seekToDiskPage runInfo.hasFS (nextPageNo pageEnd) readerKOpsHandle
-                    nextPage <- readDiskPage runInfo.hasFS readerKOpsHandle
-                    pure (nextPage, 0)
 
 {-# SPECIALISE close ::
      Keys IO h
@@ -196,53 +154,5 @@ next reader = do
             pure $ Key key
           IndexEntryOverflow key _entry lenSuffix -> do
             modifyPrimVar reader.currentEntryNo (+1)
-            skipOverflowPages reader.hasFS reader.kOpsHandle lenSuffix
+            skipPages reader.hasFS reader.kOpsHandle lenSuffix
             pure $ Key key
-
-{-------------------------------------------------------------------------------
-  Utilities
--------------------------------------------------------------------------------}
-
-seekToDiskPage :: HasFS m h -> PageNo -> FS.Handle h -> m ()
-seekToDiskPage fs pageNo h = do
-    FS.hSeek fs h FS.AbsoluteSeek (pageNoToByteOffset pageNo)
-  where
-    pageNoToByteOffset (PageNo n) =
-        assert (n >= 0) $
-          mulPageSize (fromIntegral n)
-
-{-# SPECIALISE readDiskPage ::
-     HasFS IO h
-  -> FS.Handle h
-  -> IO (Maybe RawPage) #-}
--- | Returns 'Nothing' on EOF.
-readDiskPage ::
-     (MonadCatch m, PrimMonad m)
-  => HasFS m h
-  -> FS.Handle h
-  -> m (Maybe RawPage)
-readDiskPage fs h = do
-    mba <- newPinnedByteArray pageSize
-    -- TODO: make sure no other exception type can be thrown
-    --
-    -- TODO: if FS.FsReachEOF is thrown as an injected disk fault, then we
-    -- incorrectly deduce that the file has no more contents. We should probably
-    -- use an explicit file pointer instead in the style of 'FilePointer'.
-    handleJust (guard . FS.isFsErrorType FS.FsReachedEOF) (\_ -> pure Nothing) $ do
-      bytesRead <- FS.hGetBufExactly fs h mba 0 (fromIntegral pageSize)
-      assert (fromIntegral bytesRead == pageSize) $ pure ()
-      ba <- unsafeFreezeByteArray mba
-      let !rawPage = unsafeMakeRawPage ba 0
-      pure (Just rawPage)
-
-pageSize :: Int
-pageSize = 4096
-
-skipOverflowPages ::
-      HasFS m h
-   -> FS.Handle h
-   -> Word32
-   -> m ()
-skipOverflowPages fs h len = do
-    let lenPages = fromIntegral (roundUpToPageSize len)  -- always read whole pages
-    FS.hSeek fs h FS.RelativeSeek lenPages
